ee7551530d911262c6190f0952452bb4a362705f
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-08-04 15:07:33 +0100

    added framework for integration tests

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.UserContext/Claims.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.UserContext/Claims.cs
@@ -3,8 +3,8 @@
 	public abstract class Claims
 	{
 		public const string ClaimPrefix = "concerns-casework.";
-		public const string CaseWorkerRoleClaim = $"{ClaimPrefix}caseworker";
-		public const string TeamLeaderRoleClaim = $"{ClaimPrefix}teamleader";
-		public const string AdminRoleClaim = $"{ClaimPrefix}admin";
+		public const string CaseWorkerRoleClaim = $"caseworker";
+		public const string TeamLeaderRoleClaim = $"teamleader";
+		public const string AdminRoleClaim = $"admin";
 	}
 }
