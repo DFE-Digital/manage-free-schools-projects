ec8634fae733d2893b16ae84ca94ab2b50d98437
Zach Hodges <Zach.HODGES@EDUCATION.GOV.UK>
2023-09-26 12:04:10 +0100

    WIP - initial code for sending email via Gov Notify

---
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API/Controllers/EmailController.cs
@@ -0,0 +1,41 @@
+﻿using Dfe.ManageFreeSchoolProjects.API.UseCases.Email;
+using Dfe.ManageFreeSchoolProjects.Logging;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Dfe.ManageFreeSchoolProjects.API.Controllers;
+
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/email")]
+[ApiController]
+public class EmailController : ControllerBase
+{
+    private readonly IEmailService _emailService;
+    private readonly ILogger<EmailController> _logger;
+
+    public EmailController(IEmailService emailService, ILogger<EmailController> logger)
+    {
+        _emailService = emailService;
+        _logger = logger;
+    }
+
+     [HttpPost]
+     public async Task<ActionResult> SendEmail(string email)
+     {
+         _logger.LogMethodEntered();
+
+         if (email.IsNullOrEmpty()) return BadRequest("Email is required.");
+         
+         try
+         {
+             await _emailService.SendEmail(email, "4cc18c16-5535-4498-a06f-2b3d1e3eb57b");
+             return Ok();
+         }
+         catch (Exception ex)
+         {
+             _logger.LogErrorMsg(ex);
+             return new ObjectResult("Failed to send email. Check Gov Notify service.")
+                 { StatusCode = StatusCodes.Status500InternalServerError };
+         }
+     }
+}
---
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API/UseCases/Email/EmailService.cs
@@ -0,0 +1,30 @@
+﻿using Notify.Client;
+using Notify.Models.Responses;
+
+namespace Dfe.ManageFreeSchoolProjects.API.UseCases.Email;
+
+
+public interface IEmailService
+{
+    Task<EmailNotificationResponse> SendEmail(string email, string templateId);
+}
+
+public class EmailService : IEmailService
+{
+    //TODO: set this, where will we store & retrieve it for Dev & Prod?
+    private const string _apiKey = "";
+
+    public Task<EmailNotificationResponse> SendEmail(string email, string templateId)
+    {
+        try
+        {
+            var client = new NotificationClient(_apiKey);
+            return client.SendEmailAsync(email, templateId);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Sending Email through Gov Notify failed.");
+            throw;
+        }    
+    }
+}
