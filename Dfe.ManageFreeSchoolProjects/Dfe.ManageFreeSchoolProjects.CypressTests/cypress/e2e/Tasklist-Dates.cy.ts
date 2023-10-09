92eb36888b178300cae8f3738fe7d0a6d090127f
GOOD <Dan.GOOD@EDUCATION.GOV.UK>
2023-10-09 17:22:34 +0100

    Have made a decent start to the Tasklist-Dates.cy.ts Cypress specfile.  Have added more functions to the datesDetailsPage.ts page object

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/e2e/Tasklist-Dates.cy.ts
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/e2e/Tasklist-Dates.cy.ts
@@ -2,6 +2,7 @@ import { ProjectDetailsRequest } from "cypress/api/domain";
 import projectApi from "cypress/api/projectApi";
 import { RequestBuilder } from "cypress/api/requestBuilder";
 import { Logger } from "cypress/common/logger";
+import datesDetailsPage from "cypress/pages/datesDetailsPage";
 import datesSummaryPage from "cypress/pages/datesSummaryPage";
 import projectOverviewPage from "cypress/pages/projectOverviewPage";
 import taskListPage from "cypress/pages/taskList";
@@ -27,16 +28,56 @@ describe("Testing project overview", () => {
         Logger.log("Clicking on Task list tab");
         projectOverviewPage.selectTaskListTab();
 
-        Logger.log("Selecting Dates link from Tasklist")
+        Logger.log("Selecting Dates link from Tasklist");
         taskListPage.selectDatesFromTaskList();
 
-        Logger.log("Checking Dates Summary page elements present")
+        Logger.log("Checking Dates Summary page elements present");
         datesSummaryPage.verifyDatesSummaryElementsVisible();
 
-        Logger.log("Selecting first Change link from first 'Pre-opening' line")
+        Logger.log("Selecting first Change link from first 'Pre-opening' line");
         datesSummaryPage.selectChangePreopeningToGoToDatesDetails();
 
+        Logger.log("Attempting to save Dates Details page with no values");
+        datesDetailsPage.selectSaveAndContinueButton();
 
+        Logger.log("Check we get the correct validation messages coming back when no data entered");
+        datesDetailsPage.verifyValidationMessagesWhenNoDataSet();
+
+        Logger.log("Attempting to Reload page and clear controls");
+        cy.reload();
+        datesDetailsPage.clearTextInControls();
+
+        Logger.log("Entering in exceptional date format to check correct validation messages");
+        datesDetailsPage.enterInvalidDateFormatInEditDatesPage();
+
+        Logger.log("Submitting invalid date formats");
+        datesDetailsPage.selectSaveAndContinueButton();
+
+        Logger.log("Verify we get correct validation messages for exceptional date formats");
+        datesDetailsPage.verifyValidationMessagesWhenInvalidDateFormatEntered();
+
+        Logger.log("Attempting to Reload page and clear controls");
+        cy.reload();
+        datesDetailsPage.clearTextInControls();
+
+        Logger.log("Attempting to add invalid dates in correct format");
+        datesDetailsPage.enterInvalidDateInEditDatesPage(); 
+
+        Logger.log("Submitting invalid dates");
+        datesDetailsPage.selectSaveAndContinueButton();
+
+        Logger.log("Verify we get correct validation messages for exceptional days in dates");
+        datesDetailsPage.verifyValidationMessagesWhenInvalidDateEntered();
+
+        Logger.log("Attempting to Reload page and clear controls");
+        cy.reload();
+        datesDetailsPage.clearTextInControls();
+
+        Logger.log("Attempting to add valid dates in correct format");
+        datesDetailsPage.enterValidDatesInEditDatesPage();
+
+        Logger.log("Submitting valid dates");
+        datesDetailsPage.selectSaveAndContinueButton();
 
     });
 });
---
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/datesDetailsPage.ts
@@ -0,0 +1,152 @@
+const invalidDateFormatData = "POTATOES";
+const invalidDateDay = "31";
+const dateDay = "28";
+const dateMonth = "2";
+const dateYear = "2025";
+class DatesDetailsPage {
+    
+    
+    selectSaveAndContinueButton(): this {
+        cy.getByClass("govuk-button").click();
+        return this;
+    }
+
+    clearTextInControls(): this {
+        cy.getById("entry-into-pre-opening-day").clear();
+
+        cy.getById("entry-into-pre-opening-month").clear();
+
+        cy.getById("entry-into-pre-opening-year").clear();
+  
+        cy.getById("provisional-opening-date-agreed-with-trust-day").clear();
+
+        cy.getById("provisional-opening-date-agreed-with-trust-month").clear();
+
+        cy.getById("provisional-opening-date-agreed-with-trust-year").clear();
+
+        cy.getByTestId("opening-academic-year").clear();
+
+        cy.getByTestId("opening-academic-year-to").clear();
+
+        return this;
+    }
+
+    verifyValidationMessagesWhenNoDataSet(): this {
+        cy.getById("entry-into-pre-opening-error").should("be.visible").contains("Enter a date for the entry into pre-opening");
+
+        cy.getById("provisional-opening-date-agreed-with-trust-error").should("be.visible").contains("Enter a date for the provisional opening date agreed with trust");
+
+        cy.getById("opening-academic-year-error").should("be.visible").contains("The Opening academic year field is required.");
+
+        cy.getById("opening-academic-year-to-error").should("be.visible").contains("The OpeningAcademicYearTo field is required.");
+
+        return this;
+    }
+
+    enterInvalidDateFormatInEditDatesPage(): this {
+        cy.getById("entry-into-pre-opening-day").click();
+        cy.getById("entry-into-pre-opening-day").type(invalidDateFormatData);
+
+        cy.getById("entry-into-pre-opening-month").click();
+        cy.getById("entry-into-pre-opening-month").type(invalidDateFormatData);
+
+        cy.getById("entry-into-pre-opening-year").click();
+        cy.getById("entry-into-pre-opening-year").type(invalidDateFormatData);
+  
+        cy.getById("provisional-opening-date-agreed-with-trust-day").click();
+        cy.getById("provisional-opening-date-agreed-with-trust-day").type(invalidDateFormatData);
+
+        cy.getById("provisional-opening-date-agreed-with-trust-month").click();
+        cy.getById("provisional-opening-date-agreed-with-trust-month").type(invalidDateFormatData);
+
+        cy.getById("provisional-opening-date-agreed-with-trust-year").click();
+        cy.getById("provisional-opening-date-agreed-with-trust-year").type(invalidDateFormatData);
+
+        cy.getByTestId("opening-academic-year").click()
+        cy.getByTestId("opening-academic-year").type("2026");
+
+        cy.getByTestId("opening-academic-year-to").click()
+        cy.getByTestId("opening-academic-year-to").type("2027");
+
+        return this;
+
+    }
+
+    enterInvalidDateInEditDatesPage(): this {
+        cy.getById("entry-into-pre-opening-day").click();
+        cy.getById("entry-into-pre-opening-day").type(invalidDateDay);
+
+        cy.getById("entry-into-pre-opening-month").click();
+        cy.getById("entry-into-pre-opening-month").type(dateMonth);
+
+        cy.getById("entry-into-pre-opening-year").click();
+        cy.getById("entry-into-pre-opening-year").type(dateYear);
+  
+        cy.getById("provisional-opening-date-agreed-with-trust-day").click();
+        cy.getById("provisional-opening-date-agreed-with-trust-day").type(invalidDateDay);
+
+        cy.getById("provisional-opening-date-agreed-with-trust-month").click();
+        cy.getById("provisional-opening-date-agreed-with-trust-month").type(dateMonth);
+
+        cy.getById("provisional-opening-date-agreed-with-trust-year").click();
+        cy.getById("provisional-opening-date-agreed-with-trust-year").type(dateYear);
+
+        cy.getByTestId("opening-academic-year").click()
+        cy.getByTestId("opening-academic-year").type("2023");
+
+        cy.getByTestId("opening-academic-year-to").click()
+        cy.getByTestId("opening-academic-year-to").type("2024");
+
+        return this;
+
+    }
+
+    verifyValidationMessagesWhenInvalidDateFormatEntered(): this {
+        cy.getById("entry-into-pre-opening-error").should("be.visible").contains("Enter a date in the correct format");
+
+        cy.getById("provisional-opening-date-agreed-with-trust-error").should("be.visible").contains("Enter a date in the correct format")
+        return this;
+    }
+
+    verifyValidationMessagesWhenInvalidDateEntered(): this {
+        cy.getById("entry-into-pre-opening-error").should("be.visible").contains("Day must be between 1 and 28");
+
+        cy.getById("provisional-opening-date-agreed-with-trust-error").should("be.visible").contains("Day must be between 1 and 28");
+
+        return this;
+    }
+
+    enterValidDatesInEditDatesPage(): this {
+        cy.getById("entry-into-pre-opening-day").click();
+        cy.getById("entry-into-pre-opening-day").type(dateDay);
+
+        cy.getById("entry-into-pre-opening-month").click();
+        cy.getById("entry-into-pre-opening-month").type(dateMonth);
+
+        cy.getById("entry-into-pre-opening-year").click();
+        cy.getById("entry-into-pre-opening-year").type(dateYear);
+  
+        cy.getById("provisional-opening-date-agreed-with-trust-day").click();
+        cy.getById("provisional-opening-date-agreed-with-trust-day").type(dateDay);
+
+        cy.getById("provisional-opening-date-agreed-with-trust-month").click();
+        cy.getById("provisional-opening-date-agreed-with-trust-month").type(dateMonth);
+
+        cy.getById("provisional-opening-date-agreed-with-trust-year").click();
+        cy.getById("provisional-opening-date-agreed-with-trust-year").type(dateYear);
+
+        cy.getByTestId("opening-academic-year").click()
+        cy.getByTestId("opening-academic-year").type("2023");
+
+        cy.getByTestId("opening-academic-year-to").click()
+        cy.getByTestId("opening-academic-year-to").type("2024");
+
+
+        return this;
+    }
+   
+}
+
+const datesDetailsPage = new DatesDetailsPage();
+
+export default datesDetailsPage;
