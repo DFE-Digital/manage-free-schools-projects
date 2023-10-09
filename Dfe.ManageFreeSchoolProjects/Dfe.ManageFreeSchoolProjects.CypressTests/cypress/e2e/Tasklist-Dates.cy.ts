783cb2f6c26dd15c02c589619ce3a60aed844f98
GOOD <Dan.GOOD@EDUCATION.GOV.UK>
2023-10-09 14:24:47 +0100

    Beginning to write Tasklist-Dates.cy.ts Cypress spec file

---
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/e2e/Tasklist-Dates.cy.ts
@@ -0,0 +1,32 @@
+import { ProjectDetailsRequest } from "cypress/api/domain";
+import projectApi from "cypress/api/projectApi";
+import { RequestBuilder } from "cypress/api/requestBuilder";
+import { Logger } from "cypress/common/logger";
+import projectOverviewPage from "cypress/pages/projectOverviewPage";
+
+describe("Testing project overview", () => {
+    let project: ProjectDetailsRequest;
+
+    beforeEach(() => {
+        cy.login();
+
+        project = RequestBuilder.createProjectDetails();
+
+        projectApi
+            .post({
+                projects: [project],
+            })
+            .then(() => {
+                cy.visit(`/projects/${project.projectId}/overview`);
+            });
+    });
+
+    it("Should successfully set project dates", () => {
+        Logger.log("Clicking on Task list tab");
+
+        projectOverviewPage.selectTaskListTab()
+
+        
+
+    
+});
