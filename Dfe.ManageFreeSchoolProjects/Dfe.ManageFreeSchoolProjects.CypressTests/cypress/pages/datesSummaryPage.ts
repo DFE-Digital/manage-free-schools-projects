b42ff289d3b22938c99c7b9edcfed93e7cd2f7b7
GOOD <Dan.GOOD@EDUCATION.GOV.UK>
2023-10-09 14:50:59 +0100

    Added page object datesSummaryPage.ts page object

---
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/datesSummaryPage.ts
@@ -0,0 +1,11 @@
+class DatesSummaryPage {
+
+    public verifyDatesSummaryElementsVisible(): this {
+        
+        return this;
+    }
+}
+
+const datesSummaryPage = new DatesSummaryPage();
+
+export default datesSummaryPage;
