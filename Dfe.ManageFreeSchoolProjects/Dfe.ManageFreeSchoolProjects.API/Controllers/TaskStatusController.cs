f94df4306dca228999d562301d235032e163e7b8
Zach Hodges <Zach.HODGES@EDUCATION.GOV.UK>
2023-10-11 12:09:08 +0100

    wip - task status api

---
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API/Controllers/TaskStatusController.cs
@@ -0,0 +1,31 @@
+﻿using Dfe.ManageFreeSchoolProjects.API.Contracts.Project.Tasks;
+using Dfe.ManageFreeSchoolProjects.API.UseCases.Tasks;
+using Dfe.ManageFreeSchoolProjects.Logging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfe.ManageFreeSchoolProjects.API.Controllers;
+
+[Route("api/v{version:apiVersion}/task/status")]
+[ApiController]
+public class TaskStatusController : ControllerBase
+{
+    private readonly UpdateTaskStatusService _updateTaskStatusService;
+    private readonly ILogger<TaskStatusController> _logger;
+
+    public TaskStatusController(UpdateTaskStatusService updateTaskStatusService, ILogger<TaskStatusController> logger)
+    {
+        _updateTaskStatusService = updateTaskStatusService;
+        _logger = logger;
+    }
+
+    [HttpPatch]
+    public async Task<ActionResult> UpdateTaskStatus(string projectId, [FromBody] string taskName,
+        [FromBody] string status)
+    {
+        _logger.LogMethodEntered();
+
+        await _updateTaskStatusService.Execute(projectId, taskName, ProjectTaskStatus.Completed);
+
+        return new OkResult();
+    }
+}
