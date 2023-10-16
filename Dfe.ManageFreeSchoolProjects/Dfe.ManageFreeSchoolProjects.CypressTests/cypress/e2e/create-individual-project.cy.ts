838b6811bfde0130aa61023712426823b006fabc
GOOD <Dan.GOOD@EDUCATION.GOV.UK>
2023-10-16 16:04:50 +0100

    Added 2nd describe block so we can do positive and negative role testing in the same specfile (as we need two beforeEach blocks)

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/e2e/create-individual-project.cy.ts
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/e2e/create-individual-project.cy.ts
@@ -1,12 +1,62 @@
 import { Logger } from "cypress/common/logger";
+import { ProjectRecordCreator } from "cypress/constants/cypressConstants";
 import createProjectPage from "cypress/pages/createProjectPage";
 import validationComponent from "cypress/pages/validationComponent";
 import { v4 } from "uuid";
 
-describe("Creating an individual project", () => {
+describe("Creating an individual project - NEGATIVE ROLE TESTS", () => {
     beforeEach(() => {
-        cy.login();
-        cy.visit("/project/create/method");
+        
+        cy.login({role: "POTATO"});
+        cy.visit(Cypress.env("url"));
+        
+    });
+
+    it("Should allow the user to create a new project using the form", () => {
+        const school = `${v4()} school`;
+
+        Logger.log("Selecting method");
+        createProjectPage.continue();
+        validationComponent.hasValidationError("The method field is required");
+        createProjectPage.withMethod("Individual").continue();
+
+        Logger.log("Setting school name");
+        createProjectPage.continue();
+        validationComponent.hasValidationError(
+            "The free school name field is required",
+        );
+        createProjectPage.withSchoolExceedingLimit().continue();
+        validationComponent.hasValidationError(
+            "The free school name must be 80 characters or less",
+        );
+        createProjectPage.withSchool(school).continue();
+
+        Logger.log("Selecting region");
+        createProjectPage.continue();
+        validationComponent.hasValidationError("The region field is required");
+        createProjectPage.withRegion("SouthEast").continue();
+
+        Logger.log("Selecting local authority");
+        createProjectPage.continue();
+        validationComponent.hasValidationError(
+            "The local authority field is required",
+        );
+        createProjectPage.withLocalAuthority("Essex").continue();
+
+        Logger.log("Checking the information on the confirmation page");
+        createProjectPage
+            .hasSchool(school)
+            .hasRegion("South East")
+            .hasLocalAuthority("Essex");
+    });
+});
+
+describe("Creating an individual project - POSITIVE ROLE TESTS", () => {
+    beforeEach(() => {
+        
+        cy.login({role: "POTATO"});
+        cy.visit(Cypress.env("url"));
+        
     });
 
     it("Should allow the user to create a new project using the form", () => {
