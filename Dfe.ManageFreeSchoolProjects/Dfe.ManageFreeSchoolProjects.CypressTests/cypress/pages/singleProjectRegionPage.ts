4232d20d26376fa7a2cad9e372e0d948e6c4d208
GOOD <Dan.GOOD@EDUCATION.GOV.UK>
2023-10-19 16:31:23 +0100

    Added shell for checkElementsVisible() function to singleProjectRegionPage.ts page object

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/singleProjectRegionPage.ts
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/singleProjectRegionPage.ts
@@ -1,5 +1,9 @@
 class SingleProjectRegionPage {
-    
+    public checkElementsVisible(): this {
+        
+        
+        return this;
+    }
 }
 
 const singleProjectRegionPage = new SingleProjectRegionPage();
