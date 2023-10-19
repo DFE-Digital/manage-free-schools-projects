68a988c374b10b6abf9a727bbcf96ffaf61618a6
GOOD <Dan.GOOD@EDUCATION.GOV.UK>
2023-10-19 16:59:19 +0100

    Added submitAnswersAndGenerateProject() function to singleProjectCheckYourAnswers.ts page object

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/singleProjectCheckYourAnswersPage.ts
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/singleProjectCheckYourAnswersPage.ts
@@ -1,7 +1,11 @@
 class SingleProjectCheckYourAnswersPage {
     public checkElementsVisible(): this {
-        
-        
+
+        return this;
+    }
+
+    public submitAnswersAndGenerateProject(): this {
+        cy.contains("Create project").eq(1).click();
         return this;
     }
 }
