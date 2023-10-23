535bc0cc6c4682bc86b1e65641bab0299b746f4e
Dan Good <dangood84@me.com>
2023-10-23 09:42:21 +0100

    Completed putting all functions into singleProjectTemporaryProjectIdPage.ts page object

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/singleProjectTemporaryProjectIdPage.ts
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/singleProjectTemporaryProjectIdPage.ts
@@ -1,3 +1,5 @@
+import dataGenerator from "cypress/fixtures/dataGenerator";
+
 class SingleProjectTemporaryProjectIdPage {
     public checkElementsVisible(): this {
         cy.contains("Back");
@@ -17,7 +19,84 @@ class SingleProjectTemporaryProjectIdPage {
     public selectContinue(): this {
         cy.getByTestId("continue").click();
 
+        return this;
     }
+
+    public verifyEmptyValidationMessage(): this {
+        cy.getById("projectid-error-link").contains("The temporary project id field is required");
+        cy.getById("projectid-error").contains("The temporary project id field is required");
+
+        return this;
+    }
+
+    public UserEntersAndSubmitsInvalidChars(): this {
+        cy.getByTestId("projectid").type(",\"(){}<>,!@£$%^&*+-");
+        cy.getByTestId("continue").click();
+
+        return this;
+    }
+
+    public verifyInvalidCharsValidationMessage(): this {
+        cy.getById("projectid-error-link").contains("Temporary project ID must only include numbers and letters");
+        cy.getById("projectid-error").contains("Temporary project ID must only include numbers and letters");
+
+        return this;
+    }
+
+    public UserEntersAndSubmitsSpaces(): this {
+        cy.getByTestId("projectid").type("W 3 4 5 6 ");
+        cy.getByTestId("continue").click();
+
+        return this;
+    }
+
+    public verifySpacesValidationMessage(): this {
+        cy.getById("projectid-error-link").contains("Temporary project ID must not include spaces");
+        cy.getById("projectid-error").contains("Temporary project ID must not include spaces");
+
+        return this;
+    }
+
+    public UserEntersMoreThanTwentyFiveChars(): this {
+        cy.getByTestId("projectid").type("AB345678901234567890123456");
+        cy.getByTestId("continue").click();
+
+        return this;
+    }
+
+    public verifyMoreThanTwentFiveCharsValidationMessage(): this {
+        cy.getById("projectid-error-link").contains("Temporary project ID must not include spaces");
+        cy.getById("projectid-error").contains("Temporary project ID must not include spaces");
+
+        return this;
+    }
+
+    public UserAttemptsSQLInjection(): this {
+        cy.getByTestId("projectid").type("' OR 1=1");
+        cy.getByTestId("continue").click();
+
+        return this;
+    }
+
+    public UserAttemptsJavaScriptAttack(): this {
+        cy.getByTestId("projectid").type("<script>window.alert('Hello World!')</script>");
+        cy.getByTestId("continue").click();
+
+        return this;
+    }
+
+    public UserEntersValidTempId(): this {
+        let tempId = dataGenerator.generateTemporaryId();
+
+        cy.getByTestId("projectid").type(tempId);
+        cy.getByTestId("continue").click();
+
+        return this;
+    }
+
+
+
+
 
 }
 
