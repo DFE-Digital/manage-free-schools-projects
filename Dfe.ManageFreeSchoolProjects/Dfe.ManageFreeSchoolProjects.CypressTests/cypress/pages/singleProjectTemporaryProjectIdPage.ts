8e23e2bcc1f26c1ba032e614c839beae52676655
Dan Good <dangood84@me.com>
2023-10-23 08:19:32 +0100

    Fleshed out checkElementsVisible() function in singleProjectTemporaryProjectId.ts page object

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/singleProjectTemporaryProjectIdPage.ts
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/singleProjectTemporaryProjectIdPage.ts
@@ -1,6 +1,15 @@
 class SingleProjectTemporaryProjectIdPage {
     public checkElementsVisible(): this {
-        
+        cy.contains("Back");
+
+        cy.get("h1").contains("Creating a new free school project");
+        cy.get("h1").contains("What is the temporary project ID?");
+
+        cy.getById("create-new-project-id-hint").contains("For example, W3456");
+
+        cy.getById("projectid");
+
+        cy.getByTestId("continue");
         
         return this;
     }
