911f2502a61d5ffae604270a32e63cb8b459e977
Dan Good <dangood84@me.com>
2023-10-23 11:37:04 +0100

    Added functions to select each radio button and check just the selecte one is selected and all others are unselected

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/singleProjectRegionPage.ts
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/singleProjectRegionPage.ts
@@ -42,6 +42,201 @@ class SingleProjectRegionPage {
 
         return this;
     }
+
+    public verifyValidationMessage(): this {
+        cy.getById("region-error-link").contains("The region field is required");
+        cy.getById("region-error").contains("The The region field is required");
+
+        return this;
+    }
+
+    public selectEastMidlands(): this {
+        cy.getByTestId("EastMidlands").click();
+        cy.getByTestId("EastMidlands").should("be.checked");
+
+        cy.getByTestId("London").should("not.be.checked");
+
+        cy.getByTestId("EastOfEngland").should("not.be.checked");
+
+        cy.getByTestId("NorthEast").should("not.be.checked");
+
+        cy.getByTestId("NorthWest").should("not.be.checked");
+
+        cy.getByTestId("SouthEast").should("not.be.checked");
+
+        cy.getByTestId("SouthWest").should("not.be.checked");
+
+        cy.getByTestId("WestMidlands").should("not.be.checked");
+
+        cy.getByTestId("YorkshireAndHumber").should("not.be.checked");
+
+        return this;
+    }
+
+    public selectEastOfEngland(): this {
+        cy.getByTestId("EastOfEngland").click();
+        cy.getByTestId("EastOfEngland").should("be.checked");
+
+        cy.getByTestId("London").should("not.be.checked");
+
+        cy.getByTestId("EastMidlands").should("not.be.checked");
+
+        cy.getByTestId("NorthEast").should("not.be.checked");
+
+        cy.getByTestId("NorthWest").should("not.be.checked");
+
+        cy.getByTestId("SouthEast").should("not.be.checked");
+
+        cy.getByTestId("SouthWest").should("not.be.checked");
+
+        cy.getByTestId("WestMidlands").should("not.be.checked");
+
+        cy.getByTestId("YorkshireAndHumber").should("not.be.checked");
+
+        return this;
+    }
+
+    public selectNorthEast(): this {
+        cy.getByTestId("NorthEast").click();
+        cy.getByTestId("NorthEast").should("be.checked");
+
+        cy.getByTestId("EastOfEngland").should("not.be.checked");
+
+        cy.getByTestId("EastMidlands").should("not.be.checked");
+
+        cy.getByTestId("London").should("not.be.checked");
+
+        cy.getByTestId("NorthWest").should("not.be.checked");
+
+        cy.getByTestId("SouthEast").should("not.be.checked");
+
+        cy.getByTestId("SouthWest").should("not.be.checked");
+
+        cy.getByTestId("WestMidlands").should("not.be.checked");
+
+        cy.getByTestId("YorkshireAndHumber").should("not.be.checked");
+
+        return this;
+    }
+
+    public selectNorthWest(): this {
+        cy.getByTestId("NorthWest").click();
+        cy.getByTestId("NorthWest").should("be.checked");
+
+        cy.getByTestId("EastOfEngland").should("not.be.checked");
+
+        cy.getByTestId("EastMidlands").should("not.be.checked");
+
+        cy.getByTestId("London").should("not.be.checked");
+
+        cy.getByTestId("NorthEast").should("not.be.checked");
+
+        cy.getByTestId("SouthEast").should("not.be.checked");
+
+        cy.getByTestId("SouthWest").should("not.be.checked");
+
+        cy.getByTestId("WestMidlands").should("not.be.checked");
+
+        cy.getByTestId("YorkshireAndHumber").should("not.be.checked");
+
+        return this;
+    }
+
+    public selectSouthEast(): this {
+        cy.getByTestId("SouthEast").click();
+        cy.getByTestId("SouthEast").should("be.checked");
+
+        cy.getByTestId("EastOfEngland").should("not.be.checked");
+
+        cy.getByTestId("EastMidlands").should("not.be.checked");
+
+        cy.getByTestId("London").should("not.be.checked");
+
+        cy.getByTestId("NorthEast").should("not.be.checked");
+
+        cy.getByTestId("NorthWest").should("not.be.checked");
+
+        cy.getByTestId("SouthWest").should("not.be.checked");
+
+        cy.getByTestId("WestMidlands").should("not.be.checked");
+
+        cy.getByTestId("YorkshireAndHumber").should("not.be.checked");
+
+        return this;
+    }
+
+    public selectSouthWest(): this {
+        cy.getByTestId("SouthWest").click();
+        cy.getByTestId("SouthWest").should("be.checked");
+
+        cy.getByTestId("EastOfEngland").should("not.be.checked");
+
+        cy.getByTestId("EastMidlands").should("not.be.checked");
+
+        cy.getByTestId("London").should("not.be.checked");
+
+        cy.getByTestId("NorthEast").should("not.be.checked");
+
+        cy.getByTestId("NorthWest").should("not.be.checked");
+
+        cy.getByTestId("SouthEast").should("not.be.checked");
+
+        cy.getByTestId("WestMidlands").should("not.be.checked");
+
+        cy.getByTestId("YorkshireAndHumber").should("not.be.checked");
+
+        return this;
+    }
+
+    public selectWestMidlands(): this {
+        cy.getByTestId("WestMidlands").click();
+        cy.getByTestId("WestMidlands").should("be.checked");
+
+        cy.getByTestId("EastOfEngland").should("not.be.checked");
+
+        cy.getByTestId("EastMidlands").should("not.be.checked");
+
+        cy.getByTestId("London").should("not.be.checked");
+
+        cy.getByTestId("NorthEast").should("not.be.checked");
+
+        cy.getByTestId("NorthWest").should("not.be.checked");
+
+        cy.getByTestId("SouthEast").should("not.be.checked");
+
+        cy.getByTestId("SouthWest").should("not.be.checked");
+
+        cy.getByTestId("YorkshireAndHumber").should("not.be.checked");
+
+        return this;
+    }
+
+    public selectYorkshireAndHumber(): this {
+        cy.getByTestId("YorkshireAndHumber").click();
+        cy.getByTestId("YorkshireAndHumber").should("be.checked");
+
+        cy.getByTestId("EastOfEngland").should("not.be.checked");
+
+        cy.getByTestId("EastMidlands").should("not.be.checked");
+
+        cy.getByTestId("London").should("not.be.checked");
+
+        cy.getByTestId("NorthEast").should("not.be.checked");
+
+        cy.getByTestId("NorthWest").should("not.be.checked");
+
+        cy.getByTestId("SouthEast").should("not.be.checked");
+
+        cy.getByTestId("SouthWest").should("not.be.checked");
+
+        cy.getByTestId("WestMidlands").should("not.be.checked");
+
+        return this;
+    }
+
+
+
+    
 }
 
 const singleProjectRegionPage = new SingleProjectRegionPage();
