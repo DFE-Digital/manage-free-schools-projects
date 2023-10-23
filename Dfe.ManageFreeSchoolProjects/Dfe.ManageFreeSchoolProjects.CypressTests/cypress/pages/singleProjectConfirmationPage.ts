69b785b7c85e7be8e29d79278e65beb4989b3839
Dan Good <dangood84@me.com>
2023-10-23 17:29:22 +0100

    Fleshed out checkElementsVisible() function in singleProjectConfirmationPage.ts page object

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/singleProjectConfirmationPage.ts
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/singleProjectConfirmationPage.ts
@@ -1,7 +1,16 @@
 class SingleProjectConfirmationPage {
     public checkElementsVisible(): this {
+
+        cy.get("h1").contains("Free school project created");
+
+        cy.contains("Temporary project ID");
+
+        cy.get("p").eq(0).contains("We have sent a notification email to team leaders.");
         
-        
+        cy.get("h2").contains("What happens next?");
+
+        cy.get("p").eq(1).contains("Go back to the projects listing page and see the newly created projects.");
+
         return this;
     }
 }
