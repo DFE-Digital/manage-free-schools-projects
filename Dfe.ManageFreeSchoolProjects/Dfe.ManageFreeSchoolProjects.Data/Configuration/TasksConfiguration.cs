aabdcbf3f725fc87b8d0f715b92ae7d49cd3e6b5
Zach Hodges <Zach.HODGES@EDUCATION.GOV.UK>
2023-10-25 11:24:38 +0100

    ViewSchoolTask.cshtml now displays edited fields

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.Data/Configuration/TasksConfiguration.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.Data/Configuration/TasksConfiguration.cs
@@ -10,7 +10,7 @@ public partial class TasksConfiguration : IEntityTypeConfiguration<Tasks>
     public void Configure(EntityTypeBuilder<Tasks> builder)
     {
         builder
-            .ToTable("Tasks", "mfsp")
+            .ToTable("Tasks", "dbo")
             .HasKey(tasks => new { tasks.Rid, tasks.TaskName });
 
         builder.Property(e => e.Rid)
