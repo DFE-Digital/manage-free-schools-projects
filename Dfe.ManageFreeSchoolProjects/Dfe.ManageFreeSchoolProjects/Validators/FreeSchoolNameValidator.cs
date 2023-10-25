ade8a62470993da702787c7708d68d6943944a7e
Zach Hodges <Zach.HODGES@EDUCATION.GOV.UK>
2023-10-25 22:09:16 +0100

    Custom regex for free school name & displaying school name on ViewSchoolTask.cshtml

---
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Validators/FreeSchoolNameValidator.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Dfe.ManageFreeSchoolProjects.Models;
+
+public class FreeSchoolNameValidator : ValidationAttribute
+{
+    private const string AToZSpecialCharactersPattern = @"^[a-zA-Z0-9'(),\s]*$";
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value is null)
+            return ValidationResult.Success;
+
+        var regex = new Regex(AToZSpecialCharactersPattern);
+        var match = regex.Match((string)value);
+
+        return match.Success
+            ? ValidationResult.Success
+            : new ValidationResult("Please use valid characters. Valid character are: A-Z, apostrophes, parentheses and commas.");
+    }
+}
