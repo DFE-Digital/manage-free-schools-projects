55f8de73e9060c70f995e34234d2b70ceedbaf23
Zach Hodges <Zach.HODGES@EDUCATION.GOV.UK>
2023-11-01 15:59:19 +0000

    LAs displayin on LocalAuthority.cshtml. Wip - whether to set Map them into Enums in API contract

---
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API.Contracts/Project/LocalAuthority.cs
@@ -0,0 +1,50 @@
+﻿namespace Dfe.ManageFreeSchoolProjects.API.Contracts.Project;
+
+public record LocalAuthority
+{
+    public EastMidlands EastMidlands { get; set; }
+    public EastOfEngland EastOfEngland { get; set; }
+    public London London { get; set; }
+    public NorthEast NorthEast { get; set; }
+    public NorthWest NorthWest { get; set; }
+    public SouthEast SouthEast { get; set; }
+    public SouthWest SouthWestType { get; set; }
+    public WestMidlands WestMidlands { get; set; }
+    public YorkshireAndHumber YorkshireAndHumber { get; set; }
+}
+
+public record YorkshireAndHumber
+{
+}
+
+public record WestMidlands
+{
+}
+
+public record SouthWest
+{
+}
+
+public record SouthEast
+{
+}
+
+public record NorthWest
+{
+}
+
+public record NorthEast
+{
+}
+
+public record London
+{
+}
+
+public record EastOfEngland
+{
+}
+
+public record EastMidlands
+{
+}
--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Pages/Project/Create/Individual/LocalAuthority.cshtml.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Pages/Project/Create/Individual/LocalAuthority.cshtml.cs
@@ -2,37 +2,53 @@ using Dfe.ManageFreeSchoolProjects.API.Contracts.Project;
 using Dfe.ManageFreeSchoolProjects.Constants;
 using Dfe.ManageFreeSchoolProjects.Services;
 using Dfe.ManageFreeSchoolProjects.Services.Project;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Dfe.ManageFreeSchoolProjects.Extensions;
+using Dfe.ManageFreeSchoolProjects.Services.Dashboard;
+using System.Threading.Tasks;
 
 namespace Dfe.ManageFreeSchoolProjects.Pages.Project.Create
 {
     public class LocalAuthorityModel : PageModel
     {
         [BindProperty(Name = "local-authority")]
-        [Display(Name = "local authority")]
+        [Display(Name = "Local Authority")]
         [Required]
         public string LocalAuthority { get; set; }
+        
+        [BindProperty(Name = "local-authorities")]
+        [Required]
+        public List<string> LocalAuthorities { get; set; }
 
         private readonly ErrorService _errorService;
 
         private readonly ICreateProjectCache _createProjectCache;
+        private readonly IGetLocalAuthoritiesService _getLocalAuthoritiesService;
 
-        public LocalAuthorityModel(ErrorService errorService, ICreateProjectCache createProjectCache)
+        public LocalAuthorityModel(ErrorService errorService, ICreateProjectCache createProjectCache, IGetLocalAuthoritiesService getLocalAuthoritiesService)
         {
             _errorService = errorService;
             _createProjectCache = createProjectCache;
+            _getLocalAuthoritiesService = getLocalAuthoritiesService;
         }
 
-        public IActionResult OnGet()
+        public async Task<ActionResult> OnGet()
         {
             if (!User.IsInRole(RolesConstants.ProjectRecordCreator))
             {
                 return new UnauthorizedResult();
             }
+
+            var region = _createProjectCache.Get().Region.ToDescription();
+            var response = await _getLocalAuthoritiesService.Execute(new List<string> { region });
+
+            LocalAuthorities = response.LocalAuthorities.Select(x => x.Name).ToList(); 
+            
             return Page();
         }
 
