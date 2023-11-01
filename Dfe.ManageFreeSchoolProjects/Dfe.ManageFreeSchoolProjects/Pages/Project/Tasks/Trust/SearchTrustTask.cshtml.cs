88dae61821c8fb9d5f4fc53f44bd410857a46eef
Jack Marshall <jack.marshall@education.gov.uk>
2023-11-01 12:34:20 +0000

    Autocomplete WIP

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Pages/Project/Tasks/Trust/SearchTrustTask.cshtml.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Pages/Project/Tasks/Trust/SearchTrustTask.cshtml.cs
@@ -38,6 +38,9 @@ namespace Dfe.ManageFreeSchoolProjects.Pages.Project.Tasks.Trust
         [RegularExpression("TR\\d\\d\\d\\d\\d", ErrorMessage = "The TRN must be in the format TRXXXXX")]
         public string TRN { get; set; }
 
+        [BindProperty(Name = "selectedTrustTrn")]
+        public string SelectedTrustTrn { get; set; }
+
         public string Nonce;
 
         public GetTrustByRefResponse Trust { get; set; }
@@ -83,7 +86,7 @@ namespace Dfe.ManageFreeSchoolProjects.Pages.Project.Tasks.Trust
             try
             {
                 //Attempt to get trust, will throw an exception when 404 is returned
-                Trust = await _getTrustByRefService.Execute(TRN);
+                Trust = await _getTrustByRefService.Execute(SelectedTrustTrn);
                 
             }
             catch (HttpRequestException ex)
