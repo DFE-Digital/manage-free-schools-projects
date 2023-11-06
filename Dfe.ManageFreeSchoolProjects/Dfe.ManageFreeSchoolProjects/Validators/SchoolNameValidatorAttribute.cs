b1087d7934af5d0730f15aac67784b1341beb561
Sukhvinder Bhullar <Sukhvinder.Bhullar@EDUCATION.GOV.UK>
2023-11-06 10:45:40 +0000

    Fix: Schoolname allowed special chars need further restrictions

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Validators/SchoolNameValidatorAttribute.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Validators/SchoolNameValidatorAttribute.cs
@@ -3,12 +3,14 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text.RegularExpressions;
 using AngleSharp.Text;
+using Dfe.ManageFreeSchoolProjects.Constants;
 
 namespace Dfe.ManageFreeSchoolProjects.Models;
 
 public class SchoolNameValidatorAttribute : ValidationAttribute
 {
     private const string AllowSpecialCharactersPattern = @"^(?=.*[a-zA-Z])[a-zA-Z0-9'(),\s]*$";
+    private const int MaxLength = 100;
 
     protected override ValidationResult IsValid(object value, ValidationContext validationContext)
     {
@@ -16,17 +18,15 @@ public class SchoolNameValidatorAttribute : ValidationAttribute
             return ValidationResult.Success;
 
         var valueAsString = (string) value;
-        
-        if (!valueAsString.ToList().Exists(x => x.IsLetter()))
-        {
-            return new ValidationResult("Please enter some letters.");
-        }
-        
+
+        if(valueAsString.Length > MaxLength)
+            return new ValidationResult(string.Format(ValidationConstants.TextValidationMessage, "school name", MaxLength));
+
         var specialCharactersRegex = new Regex(AllowSpecialCharactersPattern, RegexOptions.None, TimeSpan.FromSeconds(30));
         var match = specialCharactersRegex.Match(valueAsString);
         
         return match.Success
             ? ValidationResult.Success
-            : new ValidationResult("Please use valid characters. Valid characters are: A-Z, apostrophes, parentheses and commas.");
+            : new ValidationResult("School name must not include special characters other than , ( ) '");
     }
 }
