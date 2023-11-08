1e3b07b4d8ca37c9fb407f97bf7b787449ca7714
Zach Hodges <Zach.HODGES@EDUCATION.GOV.UK>
2023-11-08 15:59:10 +0000

    wip - setting up edit pages

---
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Pages/Project/Tasks/RegionAndLA/EditRegion.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dfe.ManageFreeSchoolProjects.Pages.Project.Tasks.RegionLocalAuthority;
+
+public class EditRegion : PageModel
+{
+    [BindProperty(SupportsGet = true, Name = "projectId")]
+    public string ProjectId { get; set; }
+    public string Region { get; set; }
+    
+    public void OnGet()
+    {
+        
+    }
+}
