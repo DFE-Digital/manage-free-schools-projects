259dd2dd4f65ba379aebc5f05f285582666962ec
Zach Hodges <Zach.HODGES@EDUCATION.GOV.UK>
2023-11-08 15:08:48 +0000

    Now creating Region & LA task status. Wip - editing task & mark as complete

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API.Contracts/Project/Tasks/GetProjectByTaskResponse.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API.Contracts/Project/Tasks/GetProjectByTaskResponse.cs
@@ -1,10 +1,13 @@
-﻿namespace Dfe.ManageFreeSchoolProjects.API.Contracts.Project.Tasks
+﻿using System.ComponentModel;
+
+namespace Dfe.ManageFreeSchoolProjects.API.Contracts.Project.Tasks
 {
     public class GetProjectByTaskResponse
     {
         public DatesTask Dates { get; set; }
         public SchoolTask School { get; set; }
         public TrustTask Trust { get; set; }
+        
         public RegionAndLocalAuthorityTask RegionAndLocalAuthority { get; set; }
     }
 }
