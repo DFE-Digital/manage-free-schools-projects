0807d414b896ff3c0214db97e3199f570a4d562a
Dan Good <dangood84@me.com>
2023-11-13 08:40:15 +0000

    Added functions to taskListPage.ts page object to account for new RegionAndLA section for the tasklist

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/taskListPage.ts
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/taskListPage.ts
@@ -32,7 +32,15 @@ class TaskListPage {
 
     public verifyTrustMarkedAsComplete(): this {
         cy.getByClass("app-task-list__item").eq(2).contains("Completed");
+        return this;
     }
+
+    public verifyRegionAndLAMarkedAsComplete(): this {
+        cy.getByClass("app-task-list__item").eq(3).contains("Completed");
+        return this;
+    }
+
+
 }
 
 const taskListPage = new TaskListPage();
