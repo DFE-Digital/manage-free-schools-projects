c71593ed16c59616bd975474504a108a8af6a093
Zach Hodges <Zach.HODGES@EDUCATION.GOV.UK>
2023-11-14 11:21:00 +0000

    Fixed unit tests, need to inject vars

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API.Tests/Integration/EmailApiTest.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API.Tests/Integration/EmailApiTest.cs
@@ -1,6 +1,10 @@
 ﻿using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
 using System.Threading.Tasks;
 using Dfe.ManageFreeSchoolProjects.API.Tests.Fixtures;
+using Newtonsoft.Json;
 
 namespace Dfe.ManageFreeSchoolProjects.API.Tests.Integration;
 
@@ -11,44 +15,41 @@ public class EmailApiTest : ApiTestsBase
     {
     }
     
-    [Fact(Skip = "Will be used when required.")]
+    [Fact]
     public async Task When_Email_Empty_Or_Null_Returns_Status400_With_ErrorMsg()
     {
-        var result = await _client.PostAsync("/api/v1/email?email=", null);
+        var emptyString = ConvertToJson(string.Empty);
+        
+        var result = await _client.PostAsync("/api/v1/email", emptyString);
         var responseMessage = await result.Content.ReadAsStringAsync();
         
         result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-        responseMessage.Should().Be("Email is required.");
+        responseMessage.Should().Contain("Email is required.");
     } 
     
-    [Theory(Skip = "Will be used when required.")]
+    [Theory]
     [InlineData("@invalidemail.com")]
     [InlineData("invalid@")]
     public async Task When_Email_Is_Invalid_Returns_Status400_With_ErrorMsg(string email)
     {
-        var result = await _client.PostAsync($"/api/v1/email?email={email}", null);
+        var result = await _client.PostAsync($"/api/v1/email", ConvertToJson(email));
         var responseMessage = await result.Content.ReadAsStringAsync();
         
         result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
         responseMessage.Should().Be("Email is not valid.");
     } 
     
-    [Fact(Skip = "Will be used when required.")]
+    [Fact]
     public async Task When_Email_Valid_And_Sent_Returns_Ok()
     {
-        //Note: Use Test api key as it mimics sending email
-        var result = await _client.PostAsync($"/api/v1/email?email=test%40education.gov.uk", null);
+        var result = await _client.PostAsync($"/api/v1/email", ConvertToJson("test@education.gov.uk"));
         result.StatusCode.Should().Be(HttpStatusCode.OK);
     } 
     
-    [Fact(Skip = "Will be used when required.")]
-    public async Task When_Error_Returned_From_GovNotify_Returns_Status500_With_ErrorMsg()
+    private static StringContent ConvertToJson(string email)
     {
-        //Note: Use Test api key as it mimics sending email
-        var result = await _client.PostAsync($"/api/v1/email?email=asdsadadad%40education.gov.uk", null);
-        var responseMessage = await result.Content.ReadAsStringAsync();
+        var body = JsonConvert.SerializeObject(email);
 
-        result.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
-        responseMessage.Should().Be("Failed to send email. Check email exists/valid or Gov Notify service.");
-    } 
+        return new StringContent(body, Encoding.UTF8, MediaTypeNames.Application.Json);
+    }
 }
