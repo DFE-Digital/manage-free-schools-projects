ab4fcaa25a1af457c23255dc6f7a082b00782447
danielryannimble <danielryan@nimbleapproach.com>
2024-01-24 14:21:53 +0000

    contacts requests added

---
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API.Contracts/Project/Contacts/UpdateContactsRequest.cs
@@ -0,0 +1,6 @@
+namespace Dfe.ManageFreeSchoolProjects.API.Contracts.Project.Contacts;
+
+public class UpdateContactsRequest
+{
+    public ContactsTask Contacts { get; set; }
+}
--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API/Controllers/ContactsController.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API/Controllers/ContactsController.cs
@@ -1,5 +1,7 @@
+using Dfe.ManageFreeSchoolProjects.API.Contracts.Project.Contacts;
 using Dfe.ManageFreeSchoolProjects.API.Contracts.Project.Tasks;
 using Dfe.ManageFreeSchoolProjects.API.Contracts.ResponseModels;
+using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Contacts;
 using Dfe.ManageFreeSchoolProjects.Logging;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,26 +15,34 @@ namespace Dfe.ManageFreeSchoolProjects.API.Controllers;
 public class ContactsController : ControllerBase
 {
     private readonly ILogger<ContactsController> _logger;
+    private readonly IGetProjectContactsService _getProjectContactsService;
+    private readonly IUpdateProjectContactsService _getUpdateProjectContactsService;
 
-    public ContactsController(ILogger<ContactsController> logger)
+    public ContactsController(ILogger<ContactsController> logger, IGetProjectContactsService getProjectContactsService, IUpdateProjectContactsService updateProjectContactsService)
     {
-        
+        _logger = logger;
+        _getProjectContactsService = getProjectContactsService;
+        _getUpdateProjectContactsService = updateProjectContactsService;
     }
     
     [HttpGet]
-    public async Task<ActionResult<ApiSingleResponseV2<GetContactsByRefResponse>>> GetContacts(string projectId)
+    public async Task<ActionResult<ApiSingleResponseV2<GetContactsResponse>>> GetContacts(string projectId)
     {
         _logger.LogMethodEntered();
         
-        var response = await _getContactsService.Execute(projectId);
+        var response = await _getProjectContactsService.Execute(projectId);
         
-        return new ObjectResult(new ApiSingleResponseV2<GetContactsByRefResponse>(response))
+        return new ObjectResult(new ApiSingleResponseV2<GetContactsResponse>(response))
             { StatusCode = StatusCodes.Status200OK };
     }
     
     [HttpPatch]
-    public async Task<ActionResult<ApiSingleResponseV2<GetContactsByRefResponse>>> UpdateContacts(string projectId)
+    public async Task<ActionResult<ApiSingleResponseV2<GetContactsResponse>>> UpdateContacts(string projectId, UpdateContactsRequest request)
     {
         _logger.LogMethodEntered();
+
+        await _getUpdateProjectContactsService.Execute(projectId, request);
+
+        return new OkResult();
     }
 }
