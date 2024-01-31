5e0745d39a6f90a38dd9bb624dc9c5e8e418e3e1
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-01-31 15:50:31 +0000

    Terraform module updated to 1.4.9 (#365)
    * Update to Container App module v1.4.9
    
    * Updated packages
    
    * Updated TF to 1.7.1
    
    * Switched Health Probe Protocol to http
    
    * This ensures Front Door does not throw a health probe error when the origin is pointing to a WAF
    
    * Set forwarding protocol to HttpOnly

--- terraform/container-apps-hosting.tf
+++ terraform/container-apps-hosting.tf
@@ -1,5 +1,5 @@
 module "azure_container_apps_hosting" {
-  source = "github.com/DFE-Digital/terraform-azurerm-container-apps-hosting?ref=v1.4.8"
+  source = "github.com/DFE-Digital/terraform-azurerm-container-apps-hosting?ref=v1.4.9"
 
   environment    = local.environment
   project_name   = local.project_name
@@ -17,6 +17,7 @@ module "azure_container_apps_hosting" {
   image_tag                              = local.image_tag
   container_command                      = local.container_command
   container_secret_environment_variables = local.container_secret_environment_variables
+  container_scale_http_concurrency       = local.container_scale_http_concurrency
 
   enable_mssql_database              = local.enable_mssql_database
   mssql_server_admin_password        = local.mssql_server_admin_password
