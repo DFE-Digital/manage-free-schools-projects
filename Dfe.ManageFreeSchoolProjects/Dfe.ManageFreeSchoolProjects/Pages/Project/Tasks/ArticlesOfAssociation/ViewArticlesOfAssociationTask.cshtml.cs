7076f4ac86ed6a97e8fbdeac8aca4c6eeb53c1c2
mikestock-nimble <mike.stock@nimbleapproach.com>
2024-02-16 15:39:48 +0000

    refactored view tasks so they share common code

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Pages/Project/Tasks/ArticlesOfAssociation/ViewArticlesOfAssociationTask.cshtml.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Pages/Project/Tasks/ArticlesOfAssociation/ViewArticlesOfAssociationTask.cshtml.cs
@@ -1,76 +1,41 @@
 using Dfe.ManageFreeSchoolProjects.API.Contracts.Project.Tasks;
-using Dfe.ManageFreeSchoolProjects.Services;
+using Dfe.ManageFreeSchoolProjects.Constants;
+using Dfe.ManageFreeSchoolProjects.Logging;
 using Dfe.ManageFreeSchoolProjects.Services.Project;
 using Dfe.ManageFreeSchoolProjects.Services.Tasks;
-using Dfe.ManageFreeSchoolProjects.Logging;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
-using Dfe.ManageFreeSchoolProjects.API.Contracts.Task;
-using Dfe.ManageFreeSchoolProjects.Constants;
 
 namespace Dfe.ManageFreeSchoolProjects.Pages.Project.Tasks.ArticlesOfAssociation
 {
-    public class ViewArticlesOfAssociationTask : PageModel
+    public class ViewArticlesOfAssociationTask : ViewTaskBaseModel
     {
         private readonly ILogger<ViewArticlesOfAssociationTask> _logger;
-        private readonly IGetProjectByTaskService _getProjectService;
-        private readonly IGetTaskStatusService _getTaskStatusService;
-        private readonly IUpdateTaskStatusService _updateTaskStatusService;
-        private readonly ErrorService _errorService;
-        
-        public ProjectTaskStatus ProjectTaskStatus { get; set; }
-
-        [BindProperty(SupportsGet = true, Name = "projectId")]
-        public string ProjectId { get; set; }
-
-        [BindProperty] public bool MarkAsCompleted { get; set; }
-
-        public GetProjectByTaskResponse Project { get; set; }
 
         public ViewArticlesOfAssociationTask(
             IGetProjectByTaskService getProjectService,
             ILogger<ViewArticlesOfAssociationTask> logger,
-            IGetTaskStatusService getTaskStatusService, IUpdateTaskStatusService updateTaskStatusService,
-            ErrorService errorService)
+            IGetTaskStatusService getTaskStatusService, IUpdateTaskStatusService updateTaskStatusService) : base(getProjectService, getTaskStatusService, updateTaskStatusService)
         {
             _logger = logger;
-            _getTaskStatusService = getTaskStatusService;
-            _updateTaskStatusService = updateTaskStatusService;
-            _errorService = errorService;
-            _getProjectService = getProjectService;
         }
 
         public async Task<ActionResult> OnGet()
         {
             _logger.LogMethodEntered();
 
-            Project = await _getProjectService.Execute(ProjectId, TaskName.ArticlesOfAssociation);
-
-            var taskStatusResponse = await _getTaskStatusService.Execute(ProjectId, TaskName.ArticlesOfAssociation.ToString());
-
-            ProjectTaskStatus = taskStatusResponse.ProjectTaskStatus;
-            MarkAsCompleted = ProjectTaskStatus == ProjectTaskStatus.Completed;
+            await GetTask(TaskName.ArticlesOfAssociation);
 
             return Page();
         }
 
         public async Task<ActionResult> OnPost()
         {
-            if (!ModelState.IsValid)
-            {
-                _errorService.AddErrors(ModelState.Keys, ModelState);
-                return Page();
-            }
+            _logger.LogMethodEntered();
 
-            ProjectTaskStatus = MarkAsCompleted ? ProjectTaskStatus.Completed : ProjectTaskStatus.InProgress;
+            await PostTask(TaskName.ArticlesOfAssociation);
 
-            await _updateTaskStatusService.Execute(ProjectId, new UpdateTaskStatusRequest
-            {
-                TaskName = TaskName.ArticlesOfAssociation.ToString(),
-                ProjectTaskStatus = ProjectTaskStatus
-            });
             return Redirect(string.Format(RouteConstants.TaskList, ProjectId));
         }
     }
--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Pages/Project/Tasks/Dates/ViewDatesTask.cshtml.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Pages/Project/Tasks/Dates/ViewDatesTask.cshtml.cs
@@ -2,81 +2,40 @@
 using Dfe.ManageFreeSchoolProjects.Constants;
 using Dfe.ManageFreeSchoolProjects.Logging;
 using Dfe.ManageFreeSchoolProjects.Services.Project;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Threading.Tasks;
-using Dfe.ManageFreeSchoolProjects.API.Contracts.Task;
-using Dfe.ManageFreeSchoolProjects.Services;
 using Dfe.ManageFreeSchoolProjects.Services.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace Dfe.ManageFreeSchoolProjects.Pages.Project.Tasks.Dates
 {
-    public class ViewDatesTaskModel : PageModel
+    public class ViewDatesTaskModel : ViewTaskBaseModel
     {
         private readonly ILogger<ViewDatesTaskModel> _logger;
-        private readonly IGetTaskStatusService _getTaskStatusService;
-        private readonly IUpdateTaskStatusService _updateTaskStatusService;
-        private readonly ErrorService _errorService;
-        private readonly IGetProjectByTaskService _getProjectService;
-        private const string DatesTaskName = "Dates";
-
-        [BindProperty(SupportsGet = true, Name = "projectId")]
-        public string ProjectId { get; set; }
-
-        public string CurrentFreeSchoolName { get; set; }
-
-        [BindProperty] 
-        public bool MarkAsCompleted { get; set; }
-
-        public ProjectTaskStatus ProjectTaskStatus { get; set; }
-        
-        public GetProjectByTaskResponse Project { get; set; }
-
 
         public ViewDatesTaskModel(
             IGetProjectByTaskService getProjectService,
-            ILogger<ViewDatesTaskModel> logger, IGetTaskStatusService getTaskStatusService,
-            IUpdateTaskStatusService updateTaskStatusService,
-            ErrorService errorService)
+            ILogger<ViewDatesTaskModel> logger,
+            IGetTaskStatusService getTaskStatusService, IUpdateTaskStatusService updateTaskStatusService) : base(getProjectService, getTaskStatusService, updateTaskStatusService)
         {
             _logger = logger;
-            _errorService = errorService;
-            _getTaskStatusService = getTaskStatusService;
-            _updateTaskStatusService = updateTaskStatusService;
-            _getProjectService = getProjectService;
         }
 
-        public async Task<IActionResult> OnGet()
+        public async Task<ActionResult> OnGet()
         {
             _logger.LogMethodEntered();
 
-            Project = await _getProjectService.Execute(ProjectId, TaskName.Dates);
-
-            var taskStatusResponse = await _getTaskStatusService.Execute(ProjectId, DatesTaskName);
-            CurrentFreeSchoolName = Project.SchoolName;
-            ProjectTaskStatus = taskStatusResponse.ProjectTaskStatus;
-            MarkAsCompleted = ProjectTaskStatus == ProjectTaskStatus.Completed;
+            await GetTask(TaskName.Dates);
 
             return Page();
         }
 
         public async Task<ActionResult> OnPost()
         {
-            if (!ModelState.IsValid)
-            {
-                _errorService.AddErrors(ModelState.Keys, ModelState);
-                return Page();
-            }
+            _logger.LogMethodEntered();
 
-            ProjectTaskStatus = MarkAsCompleted ? ProjectTaskStatus.Completed : ProjectTaskStatus.InProgress;
+            await PostTask(TaskName.Dates);
 
-            await _updateTaskStatusService.Execute(ProjectId, new UpdateTaskStatusRequest
-            {
-                TaskName = DatesTaskName, ProjectTaskStatus = ProjectTaskStatus
-            });
-            
             return Redirect(string.Format(RouteConstants.TaskList, ProjectId));
         }
     }
