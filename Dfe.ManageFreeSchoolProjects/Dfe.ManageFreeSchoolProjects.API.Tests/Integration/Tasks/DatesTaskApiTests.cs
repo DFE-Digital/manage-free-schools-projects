1ef51f0ecc14f5e0b5a24f5614b663ffd33a634b
mikestock-nimble <mike.stock@nimbleapproach.com>
2024-02-16 16:10:59 +0000

    moved the task api tests into individual files
    stops conflicts when we all work on tasks

---
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API.Tests/Integration/Tasks/DatesTaskApiTests.cs
@@ -0,0 +1,47 @@
+﻿using Dfe.ManageFreeSchoolProjects.API.Contracts.Project.Tasks;
+using Dfe.ManageFreeSchoolProjects.API.Tests.Fixtures;
+using Dfe.ManageFreeSchoolProjects.API.Tests.Helpers;
+using System.Threading.Tasks;
+using System;
+
+namespace Dfe.ManageFreeSchoolProjects.API.Tests.Integration.Tasks
+{
+    [Collection(ApiTestCollection.ApiTestCollectionName)]
+    public class DatesTaskApiTests : ApiTestsBase
+    {
+        public DatesTaskApiTests(ApiTestFixture apiTestFixture) : base(apiTestFixture)
+        {
+        }
+
+        [Fact]
+        public async Task Patch_DatesTask_Returns_201()
+        {
+            var project = DatabaseModelBuilder.BuildProject();
+            var projectId = project.ProjectStatusProjectId;
+
+            using var context = _testFixture.GetContext();
+            context.Kpi.Add(project);
+            await context.SaveChangesAsync();
+
+            var DateTenDaysInFuture = new DateTime().AddDays(10);
+            var DateNineDaysInFuture = new DateTime().AddDays(9);
+
+            var request = new UpdateProjectByTaskRequest()
+            {
+                Dates = new DatesTask()
+                {
+                    DateOfEntryIntoPreopening = DateTenDaysInFuture,
+                    ProvisionalOpeningDateAgreedWithTrust = DateNineDaysInFuture,
+                    OpeningAcademicYear = "2023 2024",
+                }
+            };
+
+            var projectResponse = await _client.UpdateProjectTask(projectId, request, TaskName.Dates.ToString());
+
+            projectResponse.Dates.DateOfEntryIntoPreopening.Should().Be(DateTenDaysInFuture);
+            projectResponse.Dates.ProvisionalOpeningDateAgreedWithTrust.Should().Be(DateNineDaysInFuture);
+            projectResponse.Dates.OpeningAcademicYear.Should().Be("2023 2024");
+            projectResponse.SchoolName.Should().Be(project.ProjectStatusCurrentFreeSchoolName);
+        }
+    }
+}
---
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API.Tests/Integration/Tasks/TrustTaskApiTests.cs
@@ -0,0 +1,44 @@
+﻿using Dfe.ManageFreeSchoolProjects.API.Contracts.Project.Tasks;
+using Dfe.ManageFreeSchoolProjects.API.Tests.Fixtures;
+using Dfe.ManageFreeSchoolProjects.API.Tests.Helpers;
+using Dfe.ManageFreeSchoolProjects.API.UseCases.Project;
+using System.Threading.Tasks;
+
+namespace Dfe.ManageFreeSchoolProjects.API.Tests.Integration.Tasks
+{
+    [Collection(ApiTestCollection.ApiTestCollectionName)]
+    public class TrustTaskApiTests : ApiTestsBase
+    {
+        public TrustTaskApiTests(ApiTestFixture apiTestFixture) : base(apiTestFixture)
+        {
+        }
+
+        [Fact]
+        public async Task Patch_TrustTask_Returns_201()
+        {
+            var project = DatabaseModelBuilder.BuildProject();
+            var trust = DatabaseModelBuilder.BuildTrust();
+            var projectId = project.ProjectStatusProjectId;
+
+            using var context = _testFixture.GetContext();
+            context.Kpi.Add(project);
+            context.Trust.Add(trust);
+            await context.SaveChangesAsync();
+
+            var request = new UpdateProjectByTaskRequest()
+            {
+                Trust = new TrustTask()
+                {
+                    TRN = trust.TrustRef
+                }
+            };
+
+            var projectResponse = await _client.UpdateProjectTask(projectId, request, TaskName.Trust.ToString());
+
+            projectResponse.Trust.TRN.Should().Be(trust.TrustRef);
+            projectResponse.Trust.TrustName.Should().Be(trust.TrustsTrustName);
+            projectResponse.Trust.TrustType.Should().Be(ProjectMapper.ToTrustType(trust.TrustsTrustType));
+            projectResponse.SchoolName.Should().Be(project.ProjectStatusCurrentFreeSchoolName);
+        }
+    }
+}
