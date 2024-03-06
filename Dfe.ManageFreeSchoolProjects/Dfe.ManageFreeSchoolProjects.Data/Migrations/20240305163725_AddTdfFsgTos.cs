9cff2adb5fffbf63308969e3801d3f179fc1ea1e
Sukhvinder Bhullar <Sukhvinder.Bhullar@EDUCATION.GOV.UK>
2024-03-06 13:41:54 +0000

    Added table function to sql

---
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.Data/Migrations/20240305163725_AddTdfFsgTos.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.ManageFreeSchoolProjects.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTdfFsgTos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE FUNCTION [dbo].[tdf_FSG_TOS]  
+                (  
+                 @TOS NVARCHAR(10)
+                )  
+  
+                RETURNS TABLE  
+                AS  
+                RETURN  
+                (  
+                 SELECT fp.*  
+                 FROM [dbo].[Property] fp
+                 WHERE fp.[TOS] = @TOS  
+                )              
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DROP FUNCTION [dbo].[tdf_FSG_TOS] 
+            ");
+        }
+    }
+}
