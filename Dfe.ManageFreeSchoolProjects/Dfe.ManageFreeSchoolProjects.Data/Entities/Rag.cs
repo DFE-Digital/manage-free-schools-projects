be73a7c49c2b7a0378c18a4e60f9f7f8a617e39b
mikestock-nimble <mike.stock@nimbleapproach.com>
2024-03-11 13:12:03 +0000

    moved all the fields we added to separate partial entities

---
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.Data/Entities/Rag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dfe.ManageFreeSchoolProjects.Data.Entities.Existing
+{
+    public partial class Rag
+    {
+        public string RagRatingsGovernanceAndSuitabilityRagSummary { get; set; }
+        public string RagRatingsEducationRagSummary { get; set; }
+        public string RagRatingsRiskAppraisalFormSharepointLink { get; set; }
+        public Guid RevisionMarker { get; set; }
+
+    }
+}
