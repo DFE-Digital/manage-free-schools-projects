76c69ebec51dff6488c3dc2cc8ec5ac1d974afca
mikestock-nimble <mike.stock@nimbleapproach.com>
2024-03-21 15:35:55 +0000

    160667: added validation to create project
    project does not exist
    trust does exist
    trn is not null

---
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API/UseCases/Project/CreateProjectRequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using Dfe.ManageFreeSchoolProjects.API.Contracts.RequestModels.Projects;
+using FluentValidation;
+
+namespace Dfe.ManageFreeSchoolProjects.API.UseCases.Project
+{
+    public class CreateProjectRequestValidator : AbstractValidator<CreateProjectRequest>
+    {
+        public CreateProjectRequestValidator()
+        {
+            RuleForEach(x => x.Projects).SetValidator(new CreateProjectDetailsValidator());
+        }
+    }
+
+    public class CreateProjectDetailsValidator : AbstractValidator<ProjectDetails>
+    {
+        public CreateProjectDetailsValidator()
+        {
+            RuleFor(x => x.TRN).NotEmpty();
+        }
+    }
+}
