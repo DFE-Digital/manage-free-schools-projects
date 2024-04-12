91f2c9c8a1fd82bd0f3b9655fae3bb1d26863136
mikestock-nimble <mike.stock@nimbleapproach.com>
2024-04-12 10:51:28 +0100

    162578: added a pupil numbers summary to the project overview page

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/pupil-numbers/pupilNumbersSummaryComponent.ts
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/pupil-numbers/pupilNumbersSummaryComponent.ts
@@ -1,4 +1,35 @@
 class PupilNumbersSummaryComponent {
+
+    public hasCapacity(value: string): this {
+        cy.getByTestId("capacity").should("contain.text", value);
+
+        return this;
+    }
+
+    public hasPre16PublishedAdmissionNumber(value: string): this {
+        cy.getByTestId("pre16-published-admission-number").should("contain.text", value);
+
+        return this;
+    }
+
+    public hasPost16PublishedAdmissionNumber(value: string): this {
+        cy.getByTestId("post16-published-admission-number").should("contain.text", value);
+
+        return this;
+    }
+
+    public hasMinimumViableNumber(value: string): this {
+        cy.getByTestId("minimum-viable-number").should("contain.text", value);
+
+        return this;
+    }
+
+    public hasApplicationsReceived(value: string): this {
+        cy.getByTestId("applications-received").should("contain.text", value);
+
+        return this;
+    }
+
     public viewDetails(): this {
         cy.getByTestId("change-pupil-numbers").click();
 
