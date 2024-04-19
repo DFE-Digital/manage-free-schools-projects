6a3bc550afc688d2f506491e79a3ae6a54ae65b5
Jack Marshall <jack.marshall@nimbleapproach.com>
2024-04-19 16:06:26 +0100

    Remap fields WIP

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API/UseCases/Project/Tasks/FinancePlan/GetFinancePlanTaskService.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API/UseCases/Project/Tasks/FinancePlan/GetFinancePlanTaskService.cs
@@ -1,5 +1,6 @@
 ﻿using Dfe.ManageFreeSchoolProjects.API.Contracts.Project.Tasks;
 using Dfe.ManageFreeSchoolProjects.Data;
+using Dfe.ManageFreeSchoolProjects.Data.Entities.Existing;
 using Microsoft.EntityFrameworkCore;
 
 namespace Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Tasks.FinancePlan
@@ -14,17 +15,21 @@ namespace Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Tasks.FinancePlan
 
         }
 
-        public async Task<GetProjectByTaskResponse> Get(GetTaskServiceParameters parameters)
+        public async Task<Milestones> Get(GetTaskServiceParameters parameters)
         {
             var result = await (from kpi in parameters.BaseQuery
-                                join milestones in _context.Milestones on kpi.Rid equals milestones.Rid into joinedMilestones
-                                from milestones in joinedMilestones.DefaultIfEmpty()
-                                select new GetProjectByTaskResponse()
-                                {
-                                    FinancePlan = FinancePlanTaskBuilder.Build(milestones)
-                                }).FirstOrDefaultAsync();
+                                join milestones in _context.Milestones on kpi.Rid equals milestones.Rid
+                                into joinedMilestones
 
-            return result ?? new GetProjectByTaskResponse() { FinancePlan = new() };
+                                from milestones in joinedMilestones.DefaultIfEmpty()
+
+
+                                select milestones).FirstOrDefaultAsync(); //new GetProjectByTaskResponse()
+                                                                          //{
+                                                                          //    FinancePlan = FinancePlanTaskBuilder.Build(milestones, po)
+                                                                          //});.FirstOrDefaultAsync();
+
+            return result; //?? new GetProjectByTaskResponse() { FinancePlan = new() };
         }
     }
 }
