c3801365528c3910b2945d114a7fd6f629748a25
Sukhvinder Bhullar <Sukhvinder.Bhullar@EDUCATION.GOV.UK>
2024-04-19 13:28:32 +0100

    Fix code smells

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Models/YesNoToBoolBinder.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Models/YesNoToBoolBinder.cs
@@ -7,25 +7,9 @@ namespace Dfe.ManageFreeSchoolProjects.Models
 {
     public class YesNoToBoolBinder : IModelBinder
     {
-        private readonly ILoggerFactory _loggerFactory;
-
-        public YesNoToBoolBinder(ILoggerFactory loggerFactory)
-        {
-            _loggerFactory = loggerFactory;
-        }
-
         public Task BindModelAsync(ModelBindingContext bindingContext)
         {
-            if (bindingContext == null)
-            {
-                throw new ArgumentNullException(nameof(bindingContext));
-            }
-
-            //var modelType = bindingContext.ModelType;
-            //if (modelType != typeof(bool) || modelType != typeof(bool?))
-            //{
-            //    throw new InvalidOperationException($"Cannot bind {modelType.Name}.");
-            //}
+            ArgumentNullException.ThrowIfNull(bindingContext);
 
             var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
 
