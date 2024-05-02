69cffca5df7708a9f4dfb11976e7fa3ca25e9e4a
Jack Marshall <jack.marshall@nimbleapproach.com>
2024-05-02 11:48:23 +0100

    Update navigation for when faith status is none

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Pages/Project/Create/Individual/CreateProjectBaseModel.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Pages/Project/Create/Individual/CreateProjectBaseModel.cs
@@ -5,110 +5,124 @@ using Dfe.ManageFreeSchoolProjects.Services.Project;
 
 namespace Dfe.ManageFreeSchoolProjects.Pages.Project.Create.Individual
 {
-    public class CreateProjectBaseModel : PageModel
-    {
-        protected internal string BackLink { get; set; }
-        protected readonly ICreateProjectCache _createProjectCache;
+	public class CreateProjectBaseModel : PageModel
+	{
+		protected internal string BackLink { get; set; }
+		protected readonly ICreateProjectCache _createProjectCache;
 
-        public CreateProjectBaseModel(ICreateProjectCache createProjectCache)
-        {
-            _createProjectCache = createProjectCache;
-        }
+		public CreateProjectBaseModel(ICreateProjectCache createProjectCache)
+		{
+			_createProjectCache = createProjectCache;
+		}
 
-        public bool IsUserAuthorised()
-        {
-            return User.IsInRole(RolesConstants.ProjectRecordCreator);
-        }
+		public bool IsUserAuthorised()
+		{
+			return User.IsInRole(RolesConstants.ProjectRecordCreator);
+		}
 
-        public string GetPreviousPage(CreateProjectPageName currentPageName,
-            string routeParameter = "")
-        {
-            return currentPageName switch
-            {
-                CreateProjectPageName.LocalAuthority => RouteConstants.CreateProjectRegion,
-                CreateProjectPageName.FaithType => RouteConstants.CreateFaithStatus,
-                CreateProjectPageName.ConfirmTrustSearch => RouteConstants.CreateProjectSearchTrust,
-                CreateProjectPageName.ClassType => RouteConstants.CreateProjectSchoolType,
-                _ => DefaultPreviousRoute(currentPageName, routeParameter)
-            };           
-        }
+		public string GetPreviousPage(CreateProjectPageName currentPageName,
+			string routeParameter = "")
+		{
+			return currentPageName switch
+			{
+				CreateProjectPageName.LocalAuthority => RouteConstants.CreateProjectRegion,
+				CreateProjectPageName.FaithType => RouteConstants.CreateFaithStatus,
+				CreateProjectPageName.ProvisionalOpeningDate => PreviousProvisionalOpeningDate(),
+				CreateProjectPageName.ConfirmTrustSearch => RouteConstants.CreateProjectSearchTrust,
+				CreateProjectPageName.ClassType => RouteConstants.CreateProjectSchoolType,
+				_ => DefaultPreviousRoute(currentPageName, routeParameter)
+			};
+		}
 
-        private string DefaultPreviousRoute(CreateProjectPageName currentPageName, string routeParameter)
-        {
-            var cache = _createProjectCache.Get();
+		private string PreviousProvisionalOpeningDate()
+		{
+			var cache = _createProjectCache.Get();
 
-            if (cache.ReachedCheckYourAnswers)
-                return RouteConstants.CreateProjectCheckYourAnswers;
+			var faithStatus = cache.ReachedCheckYourAnswers ? cache.PreviousFaithStatus : cache.FaithStatus;
 
-            return currentPageName switch
-            {
-                CreateProjectPageName.ProjectId => RouteConstants.CreateProjectMethod,
-                CreateProjectPageName.SchoolName => RouteConstants.CreateProjectId,
-                CreateProjectPageName.Region => RouteConstants.CreateProjectSchool,
-                CreateProjectPageName.SearchTrust => RouteConstants.CreateProjectLocalAuthority,
-                CreateProjectPageName.SchoolType => string.Format(RouteConstants.CreateProjectConfirmTrust,
-                    routeParameter),
-                CreateProjectPageName.ClassType => RouteConstants.CreateProjectSchoolType,
-                CreateProjectPageName.SchoolPhase => RouteConstants.CreateClassType,
-                CreateProjectPageName.AgeRange => RouteConstants.CreateProjectSchoolPhase,
-                CreateProjectPageName.Capacity => RouteConstants.CreateProjectAgeRange,
-                CreateProjectPageName.FaithStatus => RouteConstants.CreateProjectCapacity,
-                CreateProjectPageName.ProvisionalOpeningDate => RouteConstants.CreateFaithType,
-                CreateProjectPageName.ProjectLead => RouteConstants.CreateProjectProvisionalOpeningDate,
-                CreateProjectPageName.CheckYourAnswers => RouteConstants.CreateProjectLead,
-                _ => throw new ArgumentOutOfRangeException($"Unsupported create project page {currentPageName}")
-            };
-        }
+			if (faithStatus == API.Contracts.Project.Tasks.FaithStatus.None)
+				return cache.ReachedCheckYourAnswers ? RouteConstants.CreateProjectCheckYourAnswers
+													 : RouteConstants.CreateFaithStatus;
 
-        public string GetNextPage(CreateProjectPageName currentPageName, string routeParameter = "")
-        {
-            return currentPageName switch
-            {
-                CreateProjectPageName.FaithStatus => NextFaithStatus(),
-                CreateProjectPageName.Region => RouteConstants.CreateProjectLocalAuthority,
-                CreateProjectPageName.SearchTrust => string.Format(RouteConstants.CreateProjectConfirmTrust, routeParameter),
-                CreateProjectPageName.SchoolType => RouteConstants.CreateClassType,
-                _ => DefaultNextRoute(currentPageName)
-            };
-        }
+			return RouteConstants.CreateFaithType;
+		}
 
-        private string NextFaithStatus()
-        {
-            var cache = _createProjectCache.Get();
+		private string DefaultPreviousRoute(CreateProjectPageName currentPageName, string routeParameter)
+		{
+			var cache = _createProjectCache.Get();
 
-            var faithStatus = cache.ReachedCheckYourAnswers ? cache.PreviousFaithStatus : cache.FaithStatus;
+			if (cache.ReachedCheckYourAnswers)
+				return RouteConstants.CreateProjectCheckYourAnswers;
 
-            if (faithStatus == API.Contracts.Project.Tasks.FaithStatus.None)
-                return cache.ReachedCheckYourAnswers ? RouteConstants.CreateProjectCheckYourAnswers
-                                                     : RouteConstants.CreateProjectProvisionalOpeningDate;
+			return currentPageName switch
+			{
+				CreateProjectPageName.ProjectId => RouteConstants.CreateProjectMethod,
+				CreateProjectPageName.SchoolName => RouteConstants.CreateProjectId,
+				CreateProjectPageName.Region => RouteConstants.CreateProjectSchool,
+				CreateProjectPageName.SearchTrust => RouteConstants.CreateProjectLocalAuthority,
+				CreateProjectPageName.SchoolType => string.Format(RouteConstants.CreateProjectConfirmTrust,
+					routeParameter),
+				CreateProjectPageName.ClassType => RouteConstants.CreateProjectSchoolType,
+				CreateProjectPageName.SchoolPhase => RouteConstants.CreateClassType,
+				CreateProjectPageName.AgeRange => RouteConstants.CreateProjectSchoolPhase,
+				CreateProjectPageName.Capacity => RouteConstants.CreateProjectAgeRange,
+				CreateProjectPageName.FaithStatus => RouteConstants.CreateProjectCapacity,
+				CreateProjectPageName.ProvisionalOpeningDate => RouteConstants.CreateFaithType,
+				CreateProjectPageName.ProjectLead => RouteConstants.CreateProjectProvisionalOpeningDate,
+				CreateProjectPageName.CheckYourAnswers => RouteConstants.CreateProjectLead,
+				_ => throw new ArgumentOutOfRangeException($"Unsupported create project page {currentPageName}")
+			};
+		}
 
-            return RouteConstants.CreateFaithType;
-        }
+		public string GetNextPage(CreateProjectPageName currentPageName, string routeParameter = "")
+		{
+			return currentPageName switch
+			{
+				CreateProjectPageName.FaithStatus => NextFaithStatus(),
+				CreateProjectPageName.Region => RouteConstants.CreateProjectLocalAuthority,
+				CreateProjectPageName.SearchTrust => string.Format(RouteConstants.CreateProjectConfirmTrust, routeParameter),
+				CreateProjectPageName.SchoolType => RouteConstants.CreateClassType,
+				_ => DefaultNextRoute(currentPageName)
+			};
+		}
 
-        private string DefaultNextRoute(CreateProjectPageName currentPageName)
-        {
-            var cache = _createProjectCache.Get();
+		private string NextFaithStatus()
+		{
+			var cache = _createProjectCache.Get();
 
-            if (cache.ReachedCheckYourAnswers)
-                return RouteConstants.CreateProjectCheckYourAnswers;
+			var faithStatus = cache.ReachedCheckYourAnswers ? cache.PreviousFaithStatus : cache.FaithStatus;
 
-            return currentPageName switch
-            {
-                CreateProjectPageName.ProjectId => RouteConstants.CreateProjectSchool,
-                CreateProjectPageName.SchoolName => RouteConstants.CreateProjectRegion,
-                CreateProjectPageName.LocalAuthority => RouteConstants.CreateProjectSearchTrust,
-                CreateProjectPageName.ConfirmTrustSearch => RouteConstants.CreateProjectSchoolType,
-                CreateProjectPageName.SchoolType => RouteConstants.CreateClassType,
-                CreateProjectPageName.ClassType => RouteConstants.CreateProjectSchoolPhase,
-                CreateProjectPageName.SchoolPhase => RouteConstants.CreateProjectAgeRange,
-                CreateProjectPageName.AgeRange => RouteConstants.CreateProjectCapacity,
-                CreateProjectPageName.Capacity => RouteConstants.CreateFaithStatus,
-                CreateProjectPageName.FaithType => RouteConstants.CreateProjectProvisionalOpeningDate,
-                CreateProjectPageName.ProvisionalOpeningDate => RouteConstants.CreateProjectLead,
-                CreateProjectPageName.ProjectLead => RouteConstants.CreateProjectCheckYourAnswers,
-                CreateProjectPageName.CheckYourAnswers => RouteConstants.CreateProjectConfirmation,
-                _ => throw new ArgumentOutOfRangeException($"Unsupported create project page {currentPageName}")
-            };
-        }
-    }
+			if (faithStatus == API.Contracts.Project.Tasks.FaithStatus.None)
+				return cache.ReachedCheckYourAnswers ? RouteConstants.CreateProjectCheckYourAnswers
+													 : RouteConstants.CreateProjectProvisionalOpeningDate;
+
+			return RouteConstants.CreateFaithType;
+		}
+
+		private string DefaultNextRoute(CreateProjectPageName currentPageName)
+		{
+			var cache = _createProjectCache.Get();
+
+			if (cache.ReachedCheckYourAnswers)
+				return RouteConstants.CreateProjectCheckYourAnswers;
+
+			return currentPageName switch
+			{
+				CreateProjectPageName.ProjectId => RouteConstants.CreateProjectSchool,
+				CreateProjectPageName.SchoolName => RouteConstants.CreateProjectRegion,
+				CreateProjectPageName.LocalAuthority => RouteConstants.CreateProjectSearchTrust,
+				CreateProjectPageName.ConfirmTrustSearch => RouteConstants.CreateProjectSchoolType,
+				CreateProjectPageName.SchoolType => RouteConstants.CreateClassType,
+				CreateProjectPageName.ClassType => RouteConstants.CreateProjectSchoolPhase,
+				CreateProjectPageName.SchoolPhase => RouteConstants.CreateProjectAgeRange,
+				CreateProjectPageName.AgeRange => RouteConstants.CreateProjectCapacity,
+				CreateProjectPageName.Capacity => RouteConstants.CreateFaithStatus,
+				CreateProjectPageName.FaithType => RouteConstants.CreateProjectProvisionalOpeningDate,
+				CreateProjectPageName.ProvisionalOpeningDate => RouteConstants.CreateProjectLead,
+				CreateProjectPageName.ProjectLead => RouteConstants.CreateProjectCheckYourAnswers,
+				CreateProjectPageName.CheckYourAnswers => RouteConstants.CreateProjectConfirmation,
+				_ => throw new ArgumentOutOfRangeException($"Unsupported create project page {currentPageName}")
+			};
+		}
+	}
 }
