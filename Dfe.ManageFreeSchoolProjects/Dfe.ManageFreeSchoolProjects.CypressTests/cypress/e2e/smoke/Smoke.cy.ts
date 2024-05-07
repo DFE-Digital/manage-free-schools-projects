2cebacc0f76ff6b41b73375407dee2f8b3418856
danielryannimble <danielryan@nimbleapproach.com>
2024-05-07 12:01:52 +0100

    further changes to initial smoke test

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/e2e/smoke/Smoke.cy.ts
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/e2e/smoke/Smoke.cy.ts
@@ -1,43 +1,23 @@
-import { ProjectDetailsRequest } from "cypress/api/domain";
-import projectApi from "cypress/api/projectApi";
-import { RequestBuilder } from "cypress/api/requestBuilder";
-import summaryPage from "cypress/pages/task-summary-base";
-import taskListPage from "cypress/pages/taskListPage";
-import riskPage from "cypress/pages/risk/projectRiskSummaryPage"
+import { Logger } from "cypress/common/logger";
+import { ProjectRecordCreator } from "cypress/constants/cypressConstants";
 
-
-describe("!!SMOKE Testing education brief", () => {
-
-    let project: ProjectDetailsRequest;
-
+describe("Pioneer smoke test", () => {
     beforeEach(() => {
-        cy.login();
+        cy.login({role: "POTATO"});
+        cy.visit(Cypress.env('url'));
+    });
+    
+    describe("Inital smoke tests to verify pipelines are running ok", () => {
+        beforeEach(() => {
+            cy.login({role: ProjectRecordCreator});
+            cy.visit('/');
+        });
 
-        project = RequestBuilder.createProjectDetails();
-
-        projectApi
-            .post({
-                projects: [project],
-            })
-            .then(() => {
-                cy.visit(`/projects/${project.projectId}/tasks`);
-            });
+        it("Should run in test enviroment only", { tags:['smoke']},() =>  {
+            Logger.log("this log should only show in test environment");
+            cy.contains('Create new projects').should('be.visible');
+            cy.executeAccessibilityTests();
+        });
     });
 
-    it("!!SMOKE Should successfully set education brief task smokey", { tags:['smoke']},() => {
-        cy.log("!!SMOKE education brief");
-        taskListPage.isTaskStatusIsNotStarted("EducationBrief")
-            .selectEducationBriefFromList();
-        
-        cy.log("!!SMOKE Confirm empty education brief");
-        summaryPage
-            .schoolNameIs(project.schoolName)
-            .titleIs("Education brief")
-            .inOrder()
-            .summaryShows("Education plan is in the education brief").IsEmpty().HasChangeLink()
-            .summaryShows("Education policies are in the education brief").IsEmpty().HasChangeLink()
-            .summaryShows("Pupil assessment and tracking history are in place").IsEmpty().HasChangeLink()
-            .summaryShows("Saved documents in Workplaces folder").IsEmpty().HasChangeLink()
-            .isNotMarkedAsComplete();
-    })
 })
