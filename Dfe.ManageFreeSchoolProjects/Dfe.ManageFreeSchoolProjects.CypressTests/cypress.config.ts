f764f3e76b3d6885ad2ce9ec518fd218739d4747
Dan Good <dangood84@me.com>
2024-05-09 09:15:15 +0100

    Readded require statement for cypress plugin in the cypress config file in cypress.config.ts on ln 44

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress.config.ts
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress.config.ts
@@ -41,6 +41,7 @@ export default defineConfig({
                     return null;
                 },
             });
+            require('@cypress/grep/src/plugin')(config);
             config.baseUrl = config.env.url;
             return config;
         },
