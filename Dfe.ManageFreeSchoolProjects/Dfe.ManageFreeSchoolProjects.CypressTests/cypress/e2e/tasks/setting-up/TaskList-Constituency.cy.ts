1d4214aaa4b36252c2613b913a811b3c7feb082d
Sukhvinder Bhullar <Sukhvinder.Bhullar@EDUCATION.GOV.UK>
2024-06-05 14:49:55 +0100

    Skip test

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/e2e/tasks/setting-up/TaskList-Constituency.cy.ts
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/e2e/tasks/setting-up/TaskList-Constituency.cy.ts
@@ -27,7 +27,7 @@ describe("Testing Constituency Task", () => {
     });
 
 
-    it("Should successfully set constituency .skip()", () => {
+    it.skip("Should successfully set constituency", () => {
 
         Logger.log("Select constituency");
         taskListPage.isTaskStatusIsNotStarted("Constituency")
