d56859d08f920578cf3afbc655f46a7a857cce3a
danielryannimble <danielryan@nimbleapproach.com>
2024-06-14 12:31:38 +0100

    new changes to status

---
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Validators/ValidYearAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using Dfe.ManageFreeSchoolProjects.Constants;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Dfe.ManageFreeSchoolProjects.Validators;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter,
+    AllowMultiple = false)]
+
+public class ValidYearAttribute : ValidationAttribute
+{
+    public ValidYearAttribute()
+    {
+        
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value is null)
+            return new ValidationResult(string.Format(ValidationConstants.YearValidationMessage, validationContext.DisplayName.ToLower()));
+        
+        var valueAsString = (string) value;
+        
+        bool isClosedNumber = int.TryParse(valueAsString, out int intYear);
+
+        if(valueAsString.IsNullOrEmpty() || !isClosedNumber)
+            return new ValidationResult(string.Format(ValidationConstants.YearValidationMessage, validationContext.DisplayName.ToLower()));
+        
+        
+        if(intYear is < 2000 or > 2050)
+            return new ValidationResult(string.Format(ValidationConstants.YearValueValidationMessage, validationContext.DisplayName.ToLower()));
+        
+        return ValidationResult.Success; 
+            
+    }
+}
---
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/ViewModels/YearInputViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Dfe.ManageFreeSchoolProjects.ViewModels
+{
+	public class YearInputViewModel
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public string Value { get; set; }
+		public string Label { get; set; }
+		public string ErrorMessage { get; set; }
+		public int Width { get; set; }
+		public string Hint { get; set; }
+		public bool HeadingLabel { get; set; }
+		public bool? BoldLabel { get; set; }
+		public string TestId { get; set; }
+		public bool AddMargin { get; set; }
+		public string InputStyles { get; set; }
+        public bool SmallLabel { get; set; }
+    }
+}
