762c766f9b3a525cf5d8fd20679757afb1cc625c
Sukhvinder Bhullar <Sukhvinder.Bhullar@EDUCATION.GOV.UK>
2024-06-18 13:52:39 +0100

    Increased size of fields

---
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.Data/Migrations/20240618124954_IncreasePupilNumberSize.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.ManageFreeSchoolProjects.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreasePupilNumberSize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Pupil numbers and capacity.No. applications accepted YR-Y6",
+                schema: "dbo",
+                table: "PO",
+                type: "varchar(4)",
+                unicode: false,
+                maxLength: 4,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(3)",
+                oldUnicode: false,
+                oldMaxLength: 3,
+                oldNullable: true)
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "POHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart")
+                .OldAnnotation("SqlServer:IsTemporal", true)
+                .OldAnnotation("SqlServer:TemporalHistoryTableName", "POHistory")
+                .OldAnnotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                .OldAnnotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .OldAnnotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Pupil numbers and capacity.No. applications accepted Y7-Y11",
+                schema: "dbo",
+                table: "PO",
+                type: "varchar(4)",
+                unicode: false,
+                maxLength: 4,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(3)",
+                oldUnicode: false,
+                oldMaxLength: 3,
+                oldNullable: true)
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "POHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart")
+                .OldAnnotation("SqlServer:IsTemporal", true)
+                .OldAnnotation("SqlServer:TemporalHistoryTableName", "POHistory")
+                .OldAnnotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                .OldAnnotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .OldAnnotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Pupil numbers and capacity.No. applications accepted Y12-Y14",
+                schema: "dbo",
+                table: "PO",
+                type: "varchar(4)",
+                unicode: false,
+                maxLength: 4,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(3)",
+                oldUnicode: false,
+                oldMaxLength: 3,
+                oldNullable: true)
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "POHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart")
+                .OldAnnotation("SqlServer:IsTemporal", true)
+                .OldAnnotation("SqlServer:TemporalHistoryTableName", "POHistory")
+                .OldAnnotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                .OldAnnotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .OldAnnotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Pupil numbers and capacity.No. applications accepted YR-Y6",
+                schema: "dbo",
+                table: "PO",
+                type: "varchar(3)",
+                unicode: false,
+                maxLength: 3,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(4)",
+                oldUnicode: false,
+                oldMaxLength: 4,
+                oldNullable: true)
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "POHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart")
+                .OldAnnotation("SqlServer:IsTemporal", true)
+                .OldAnnotation("SqlServer:TemporalHistoryTableName", "POHistory")
+                .OldAnnotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                .OldAnnotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .OldAnnotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Pupil numbers and capacity.No. applications accepted Y7-Y11",
+                schema: "dbo",
+                table: "PO",
+                type: "varchar(3)",
+                unicode: false,
+                maxLength: 3,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(4)",
+                oldUnicode: false,
+                oldMaxLength: 4,
+                oldNullable: true)
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "POHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart")
+                .OldAnnotation("SqlServer:IsTemporal", true)
+                .OldAnnotation("SqlServer:TemporalHistoryTableName", "POHistory")
+                .OldAnnotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                .OldAnnotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .OldAnnotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Pupil numbers and capacity.No. applications accepted Y12-Y14",
+                schema: "dbo",
+                table: "PO",
+                type: "varchar(3)",
+                unicode: false,
+                maxLength: 3,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(4)",
+                oldUnicode: false,
+                oldMaxLength: 4,
+                oldNullable: true)
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "POHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart")
+                .OldAnnotation("SqlServer:IsTemporal", true)
+                .OldAnnotation("SqlServer:TemporalHistoryTableName", "POHistory")
+                .OldAnnotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                .OldAnnotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .OldAnnotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+        }
+    }
+}
