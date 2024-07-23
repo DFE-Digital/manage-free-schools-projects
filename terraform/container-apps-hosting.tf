960ad4debf63752efbce9f20ad79b25a426e1524
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-07-23 14:15:24 +0100

    Set custom CDN waf rules
    * this will be used for specific cases where we want requests to bypass the rate limit policy

--- terraform/container-apps-hosting.tf
+++ terraform/container-apps-hosting.tf
@@ -1,5 +1,5 @@
 module "azure_container_apps_hosting" {
-  source = "github.com/DFE-Digital/terraform-azurerm-container-apps-hosting?ref=v1.8.3"
+  source = "github.com/DFE-Digital/terraform-azurerm-container-apps-hosting?ref=cdn-waf-rulesets"
 
   environment    = local.environment
   project_name   = local.project_name
@@ -43,6 +43,7 @@ module "azure_container_apps_hosting" {
   enable_cdn_frontdoor                            = local.enable_cdn_frontdoor
   cdn_frontdoor_forwarding_protocol               = local.cdn_frontdoor_forwarding_protocol
   cdn_frontdoor_enable_rate_limiting              = local.cdn_frontdoor_enable_rate_limiting
+  cdn_frontdoor_waf_custom_rules                  = local.cdn_frontdoor_waf_custom_rules
   cdn_frontdoor_rate_limiting_duration_in_minutes = local.cdn_frontdoor_rate_limiting_duration_in_minutes
   cdn_frontdoor_rate_limiting_threshold           = local.cdn_frontdoor_rate_limiting_threshold
   cdn_frontdoor_host_add_response_headers         = local.cdn_frontdoor_host_add_response_headers
