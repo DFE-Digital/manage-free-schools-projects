922c88f8588bfa641aafceee3ebaee991aa6d322
Jack Marshall <jack.marshall@nimbleapproach.com>
2024-07-30 10:06:38 +0100

    Added payment info contract for new PDG API changes

---
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API.Contracts/Project/Tasks/PDG/PaymentInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace Dfe.ManageFreeSchoolProjects.API.Contracts.Project.Tasks.PDG
+{
+    public class PaymentInfoForProject
+    {
+        public string ProjectId { get; set; }
+        public List<PaymentInfo> Payments { get; set; } = new();
+    }
+    public class PaymentInfo
+    {
+        public decimal? PaymentScheduleAmount { get; set; }
+        public DateTime? PaymentScheduleDate { get; set; }
+        public decimal? PaymentActualAmount { get; set; }
+        public DateTime? PaymentActualDate { get; set; }
+    }
+
+}
