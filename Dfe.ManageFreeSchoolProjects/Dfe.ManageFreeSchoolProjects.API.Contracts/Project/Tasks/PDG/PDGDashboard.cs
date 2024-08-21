a7103757281ca37e98bbf00d294119a7e96a5a68
Zach Hodges <zach.hodges@nimbleapproach.com>
2024-08-21 12:55:57 +0100

    fix back link

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API.Contracts/Project/Tasks/PDG/PDGDashboard.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API.Contracts/Project/Tasks/PDG/PDGDashboard.cs
@@ -4,6 +4,7 @@ namespace Dfe.ManageFreeSchoolProjects.API.Contracts.Project.Tasks.PDG
 {
     public class PDGDashboard
     {
+        public decimal? TotalGrant { get; set; }
         public decimal? PaymentScheduleAmount{ get; set; }
         public DateTime? PaymentScheduleDate { get; set; }
         public decimal? PaymentActualAmount { get; set; }
--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Pages/Project/Tasks/PDG/Central/EditPDGTotalGrant.cshtml.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Pages/Project/Tasks/PDG/Central/EditPDGTotalGrant.cshtml.cs
@@ -1,14 +1,32 @@
+using Dfe.ManageFreeSchoolProjects.Services.Project;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace Dfe.ManageFreeSchoolProjects.Pages.Project.Tasks.PDG.Central;
 
 public class EditPDGTotalGrant : PageModel
 {
-    public void OnGet()
+    public string CurrentFreeSchoolName { get; set; }
+    
+    [BindProperty(SupportsGet = true, Name = "projectId")]
+    public string ProjectId { get; set; }
+    public decimal? TotalGrantAmount { get; set; }
+
+    private string InitialGrant { get; set; }
+    private string RevisedGrant { get; set; }
+    private readonly IGetProjectByTaskService _getProjectService;
+    private readonly ILogger<EditPDGPaymentScheduleModel> _logger;
+
+
+    public EditPDGTotalGrant(IGetProjectByTaskService getProjectService, ILogger<EditPDGPaymentScheduleModel> logger)
     {
-        
+        _getProjectService = getProjectService;
+        _logger = logger;
     }
 
-    public object CurrentFreeSchoolName { get; set; }
-    public object ProjectId { get; }
+
+    public void OnGet()
+    {
+    }
 }
