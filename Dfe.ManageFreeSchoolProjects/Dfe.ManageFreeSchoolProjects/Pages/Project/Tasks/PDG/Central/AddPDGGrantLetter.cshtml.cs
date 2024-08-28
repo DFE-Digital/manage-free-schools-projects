e795438bd9924d9fbd5776fd386d39af3afd496b
Zach Hodges <zach.hodges@nimbleapproach.com>
2024-08-28 10:06:09 +0100

    wip

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Pages/Project/Tasks/PDG/Central/AddPDGGrantLetter.cshtml.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Pages/Project/Tasks/PDG/Central/AddPDGGrantLetter.cshtml.cs
@@ -1,11 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Dfe.ManageFreeSchoolProjects.Pages.Project.Tasks.PDG.Central;
 
 public class AddPDGGrantLetter : PageModel
 {
+    [BindProperty(SupportsGet = true, Name = "projectId")]
+    public string ProjectId { get; set; }
+    public string CurrentFreeSchoolName { get; set; }
     public void OnGet()
     {
         
     }
+
 }
