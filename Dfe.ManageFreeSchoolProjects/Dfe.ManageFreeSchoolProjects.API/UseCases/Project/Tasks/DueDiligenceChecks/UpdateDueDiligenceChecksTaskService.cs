aed96de8faf4f531050926fed54ed832f9680c38
Zach Hodges <zach.hodges@nimbleapproach.com>
2024-09-06 11:43:06 +0100

    wip update logic

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API/UseCases/Project/Tasks/DueDiligenceChecks/UpdateDueDiligenceChecksTaskService.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API/UseCases/Project/Tasks/DueDiligenceChecks/UpdateDueDiligenceChecksTaskService.cs
@@ -1,6 +1,26 @@
+using Azure.Core;
+using Dfe.ManageFreeSchoolProjects.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Tasks.DueDiligenceChecks;
 
-public class UpdateDueDiligenceChecksTaskService
+public class UpdateDueDiligenceChecksTaskService(MfspContext context) : IUpdateTaskService
 {
-    
+    public async Task Update(UpdateTaskServiceParameters parameters)
+    {
+        var task = parameters.Request.DueDiligenceChecks;
+        var dbKpi = parameters.Kpi;
+
+        if (task is null)
+            return;
+
+        var db = await context.Milestones.FirstOrDefaultAsync(r => r.Rid == dbKpi.Rid);
+
+        if (db == null)
+        {
+            db = new Data.Entities.Existing.Milestones { Rid = dbKpi.Rid };
+            context.Add(db);
+        }
+        
+    }
 }
