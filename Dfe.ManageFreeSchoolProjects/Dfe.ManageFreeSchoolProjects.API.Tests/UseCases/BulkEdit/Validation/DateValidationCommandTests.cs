18e1ed37d6ac9d6024e91e7467dcecba63999265
Zach Hodges <zach.hodges@nimbleapproach.com>
2024-10-15 15:33:12 +0100

    remove comments

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API.Tests/UseCases/BulkEdit/Validation/DateValidationCommandTests.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API.Tests/UseCases/BulkEdit/Validation/DateValidationCommandTests.cs
@@ -7,14 +7,14 @@ namespace Dfe.ManageFreeSchoolProjects.API.Tests.UseCases.BulkEdit.Validation
         [Theory]
         [InlineData("g", "Enter a valid date. For example, 27/03/2021")]
         [InlineData("12", "Enter a valid date. For example, 27/03/2021")]
-        [InlineData("12-13-2025", "Enter a valid date. For example, 27/03/2021")]  // Invalid format (should be dd/MM/yyyy)
-        [InlineData("12/10/2051", "Year must be between 2000 and 2050")]          // Year out of range
-        [InlineData("12/10/1999", "Year must be between 2000 and 2050")]          // Year out of range
-        [InlineData("31/04/2024", "Day must be between 1 and 30 for the given month.")] // Invalid day for April
-        [InlineData("29/02/2023", "Day must be between 1 and 28 for the given month.")] // Not a leap year
-        [InlineData("00/10/2025", "Day must be a valid number between 1 and 31")]  // Day is zero
-        [InlineData("12/00/2025", "Month must be a valid number between 1 and 12")]// Month is zero
-        [InlineData("12/13/2025", "Month must be a valid number between 1 and 12")]// Month out of range
+        [InlineData("12-13-2025", "Enter a valid date. For example, 27/03/2021")] 
+        [InlineData("12/10/2051", "Year must be between 2000 and 2050")]         
+        [InlineData("12/10/1999", "Year must be between 2000 and 2050")]         
+        [InlineData("31/04/2024", "Day must be between 1 and 30 for the given month.")] 
+        [InlineData("29/02/2023", "Day must be between 1 and 28 for the given month.")] 
+        [InlineData("00/10/2025", "Day must be a valid number between 1 and 31")]  
+        [InlineData("12/00/2025", "Month must be a valid number between 1 and 12")]
+        [InlineData("12/13/2025", "Month must be a valid number between 1 and 12")]
         public void DateValidationFails(string date, string error)
         {
             var dateValidation = new DateValidationCommand();
@@ -25,10 +25,10 @@ namespace Dfe.ManageFreeSchoolProjects.API.Tests.UseCases.BulkEdit.Validation
         }
 
         [Theory]
-        [InlineData("12/10/2025")]  // Valid date
-        [InlineData("29/02/2024")]  // Leap year valid date
-        [InlineData("31/12/2050")]  // Last valid day in the range
-        [InlineData("01/01/2000")]  // First valid day in the range
+        [InlineData("12/10/2025")]  
+        [InlineData("29/02/2024")]  
+        [InlineData("31/12/2050")]  
+        [InlineData("01/01/2000")]
         public void DateValidationPasses(string date)
         {
             var dateValidation = new DateValidationCommand();
