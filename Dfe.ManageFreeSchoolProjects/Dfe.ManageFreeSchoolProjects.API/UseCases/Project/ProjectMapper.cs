f8c18a5e65b787df74de7e4727620b404bb8f728
Jack Marshall <jack.marshall@nimbleapproach.com>
2024-10-28 14:08:33 +0000

    Corrected SchoolPhase.SixteenToNineteen string value

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API/UseCases/Project/ProjectMapper.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API/UseCases/Project/ProjectMapper.cs
@@ -41,7 +41,7 @@ namespace Dfe.ManageFreeSchoolProjects.API.UseCases.Project
             {
                 "Primary" => SchoolPhase.Primary,
                 "Secondary" => SchoolPhase.Secondary,
-                "16 to 19" => SchoolPhase.SixteenToNineteen,
+                "16 - 19" => SchoolPhase.SixteenToNineteen,
                 "All-Through" => SchoolPhase.AllThrough,
                 "All-through" => SchoolPhase.AllThrough,
                 _ => SchoolPhase.NotSet
@@ -54,7 +54,7 @@ namespace Dfe.ManageFreeSchoolProjects.API.UseCases.Project
             {
                 SchoolPhase.Primary => "Primary",
                 SchoolPhase.Secondary => "Secondary",
-                SchoolPhase.SixteenToNineteen => "16 to 19",
+                SchoolPhase.SixteenToNineteen => "16 - 19",
                 SchoolPhase.AllThrough => "All-Through",
                 _ => "NotSet"
             };
