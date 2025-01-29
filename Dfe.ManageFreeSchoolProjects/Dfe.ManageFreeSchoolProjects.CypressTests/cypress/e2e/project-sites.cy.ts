3b662d317f5341d3508b944e3119118b09de3217
Jack Marshall <jack.marshall@nimbleapproach.com>
2025-01-29 15:39:01 +0000

    Added cypress tests - can't get data as it is imported in prod so just checking for field headings

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/e2e/project-sites.cy.ts
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/e2e/project-sites.cy.ts
@@ -3,7 +3,6 @@ import projectApi from "cypress/api/projectApi";
 import { RequestBuilder } from "cypress/api/requestBuilder";
 import { Logger } from "cypress/common/logger";
 import projectOverviewPage from "cypress/pages/projectOverviewPage";
-import editSiteInformationPage from "cypress/pages/siteInformation/editSiteInformationPage";
 import viewSiteInformationPage from "cypress/pages/siteInformation/viewSiteInformationPage";
 import validationComponent from "cypress/pages/validationComponent";
 
@@ -24,131 +23,44 @@ describe("Testing the setting up of project sites - presumption route", () => {
             });
     });
 
-    it("Should be able to configure the project sites", () => {
-        Logger.log("When there are no project sites should display empty");
+    it("Should be able to open the project sites", () => {
+        Logger.log("Should be able to see all field names");
         projectOverviewPage
             .selectSiteInformationTab();
         
         viewSiteInformationPage
             .hasProjectTitleHeader(project.schoolName)
             .hasProjectStatus("Pre-opening")
-            .hasTemporarySiteAddress("Empty", "", "")
-            .hasTemporarySitePostcode("Empty")
-            .hasPermanentSiteAddress("Empty", "", "")
-            .hasPermanentSitePostcode("Empty");
+            .hasTemporaryAddress()
+            .hasTemporaryPostcode()
+            .hasTemporarySiteRiskRating()
+            .hasTemporaryPlanningDecision()
+            .hasTemporaryKeyDatesHeading()
+            .hasTemporaryKeyDates()
+            .hasTemporaryForecastDates()
+            .hasTemporaryActualDates()
+            .hasTemporaryHeadsOfTermsAgreed()
+            .hasTemporaryContractorAppointed()
+            .hasTemporaryPlanningDecisionDate()
+            .hasTemporaryAccomodationFirstReadyForOccupation()
 
-        Logger.log("Test we can submit a blank site");
+            .hasPermanentAddress()
+            .hasPermanentPostcode()
+            .hasPermanentSiteRiskRating()
+            .hasPermanentPlanningDecision()
+            .hasPermanentKeyDatesHeading()
+            .hasPermanentKeyDates()
+            .hasPermanentForecastDates()
+            .hasPermanentActualDates()
+            .hasPermanentHeadsOfTermsAgreed()
+            .hasPermanentContractorAppointed()
+            .hasPermanentPlanningDecisionDate()
+            .hasPermanentAccomodationFirstReadyForOccupation()
 
-        viewSiteInformationPage.changePermanentSite();
-
-        editSiteInformationPage.saveAndContinue();
-
-        viewSiteInformationPage
-            .hasTemporarySiteAddress("Empty", "", "")
-            .hasTemporarySitePostcode("Empty")
-            .hasTemporarySiteDatePlanningPermissionObtained("Empty")
-            .hasTemporarySiteStartDateOfOccupation("Empty")
-            .hasPermanentSiteAddress("Empty", "", "")
-            .hasPermanentSitePostcode("Empty")
-            .hasPermanentSiteDatePlanningPermissionObtained("Empty")
-            .hasPermanentSiteStartDateOfOccupation("Empty");
-
-        cy.executeAccessibilityTests();
-
-        Logger.log("Add sites to the project");
-        viewSiteInformationPage
-            .changePermanentSite();
-
-        editSiteInformationPage
-            .withFieldsExceedingMaxLength()
-            .withStartDateOfSiteOccupation("02", "", "")
-            .withDatePlanningPermissionObtained("01", "", "")
-            .saveAndContinue();
-
-        validationComponent
-            .hasValidationError("Postcode must be 10 characters or less")
-            .hasValidationError("Address line 1 must be 100 characters or less")
-            .hasValidationError("Town or city must be 100 characters or less")
-            .hasValidationError("Address line 2 must be 300 characters or less")
-            .hasValidationError("Date planning permission obtained must include a month and year")
-            .hasValidationError("Start date of site occupation must include a month and year");
+            .hasCapitalRating()
+            .hasRiskSummary();
 
         cy.executeAccessibilityTests();
-
-        editSiteInformationPage
-            .withAddressLine1("Permanent test house")
-            .withAddressLine2("Permanent test street")
-            .withTownOrCity("Permanent test town")
-            .withPostcode("TE1 1ST")
-            .withStartDateOfSiteOccupation("01", "05", "2025")
-            .withDatePlanningPermissionObtained("25", "08", "2025")
-            .saveAndContinue();
-
-        viewSiteInformationPage
-            .changeTemporarySite();
-
-        editSiteInformationPage
-            .withAddressLine1("Temporary test house")
-            .withAddressLine2("Temporary test street")
-            .withTownOrCity("Temporary test town")
-            .withPostcode("TE1 2ND")
-            .withStartDateOfSiteOccupation("22", "10", "2025")
-            .withDatePlanningPermissionObtained("23", "11", "2025")
-            .saveAndContinue();
-
-        viewSiteInformationPage
-            .hasPermanentSiteAddress("Permanent test house", "Permanent test street", "Permanent test town")
-            .hasPermanentSitePostcode("TE1 1ST")
-            .hasPermanentSiteStartDateOfOccupation("1 May 2025")
-            .hasPermanentSiteDatePlanningPermissionObtained("25 August 2025")
-            .hasTemporarySiteAddress("Temporary test house", "Temporary test street", "Temporary test town")
-            .hasTemporarySiteStartDateOfOccupation("22 October 2025")
-            .hasTemporarySiteDatePlanningPermissionObtained("23 November 2025")
-            .hasTemporarySitePostcode("TE1 2ND");
-
-        Logger.log("Change sites on the project");
-
-        viewSiteInformationPage
-            .changePermanentSite();
-
-        editSiteInformationPage
-            .withAddressLine1("Alternative permanent site")
-            .withAddressLine2("Alternative permanent street")
-            .withTownOrCity("Alternative permanent town")
-            .withPostcode("TE1 4TH")
-            .withStartDateOfSiteOccupation("15", "06", "2026")
-            .withDatePlanningPermissionObtained("16", "07", "2026")
-            .saveAndContinue();
-
-        viewSiteInformationPage
-            .changeTemporarySite();
-
-        editSiteInformationPage
-            .withAddressLine1("Alternative temporary site")
-            .withAddressLine2("Alternative temporary street")
-            .withTownOrCity("Alternative temporary town")
-            .withPostcode("TE1 3RD")
-            .withStartDateOfSiteOccupation("12", "12", "2026")
-            .withDatePlanningPermissionObtained("13", "04", "2027")
-            .saveAndContinue();
-
-        viewSiteInformationPage
-            .hasPermanentSiteAddress("Alternative permanent site", "Alternative permanent street", "Alternative permanent town")
-            .hasPermanentSitePostcode("TE1 4TH")
-            .hasPermanentSiteStartDateOfOccupation("15 June 2026")
-            .hasPermanentSiteDatePlanningPermissionObtained("16 July 2026")
-            .hasTemporarySiteAddress("Alternative temporary site", "Alternative temporary street", "Alternative temporary town")
-            .hasTemporarySitePostcode("TE1 3RD")
-            .hasTemporarySiteStartDateOfOccupation("12 December 2026")
-            .hasTemporarySiteDatePlanningPermissionObtained("13 April 2027");
-
-        Logger.log("Check the overview reflects the changes");
-
-        viewSiteInformationPage
-            .hasPermanentSiteAddress("Alternative permanent site", "Alternative permanent street", "Alternative permanent town")
-            .hasPermanentSitePostcode("TE1 4TH")
-            .hasTemporarySiteAddress("Alternative temporary site", "Alternative temporary street", "Alternative temporary town")
-            .hasTemporarySitePostcode("TE1 3RD");
     });
 
 });
@@ -169,28 +81,39 @@ describe("Testing the setting up of project sites - central route", () => {
             });
     });
 
-    it("Should be not allow user to able to edit the project sites for a central route project", () => {
-        Logger.log("When there are no project sites should display empty");
+    it("Should be able to view site information for a central route project", () => {
+        Logger.log("Should be able to see all field names");
         viewSiteInformationPage
             .hasProjectTitleHeader(project.schoolName)
             .hasProjectStatus("Pre-opening")
-            .hasTemporarySiteAddress("Empty", "", "")
-            .hasTemporarySitePostcode("Empty")
-            .hasPermanentSiteAddress("Empty", "", "")
-            .hasPermanentSitePostcode("Empty");
+            .hasTemporaryAddress()
+            .hasTemporaryPostcode()
+            .hasTemporarySiteRiskRating()
+            .hasTemporaryPlanningDecision()
+            .hasTemporaryKeyDatesHeading()
+            .hasTemporaryKeyDates()
+            .hasTemporaryForecastDates()
+            .hasTemporaryActualDates()
+            .hasTemporaryHeadsOfTermsAgreed()
+            .hasTemporaryContractorAppointed()
+            .hasTemporaryPlanningDecisionDate()
+            .hasTemporaryAccomodationFirstReadyForOccupation()
 
-        viewSiteInformationPage
-            .checkInsetTextExists()
-            .hasTemporarySiteAddress("Empty", "", "")
-            .hasTemporarySitePostcode("Empty")
-            .hasTemporarySiteDatePlanningPermissionObtained("Empty")
-            .hasTemporarySiteStartDateOfOccupation("Empty")
-            .changeTemporarySiteShouldNotExist()
-            .hasPermanentSiteAddress("Empty", "", "")
-            .hasPermanentSitePostcode("Empty")
-            .hasPermanentSiteDatePlanningPermissionObtained("Empty")
-            .hasPermanentSiteStartDateOfOccupation("Empty")
-            .changePermanentSiteShouldNotExist();
+            .hasPermanentAddress()
+            .hasPermanentPostcode()
+            .hasPermanentSiteRiskRating()
+            .hasPermanentPlanningDecision()
+            .hasPermanentKeyDatesHeading()
+            .hasPermanentKeyDates()
+            .hasPermanentForecastDates()
+            .hasPermanentActualDates()
+            .hasPermanentHeadsOfTermsAgreed()
+            .hasPermanentContractorAppointed()
+            .hasPermanentPlanningDecisionDate()
+            .hasPermanentAccomodationFirstReadyForOccupation()
+
+            .hasCapitalRating()
+            .hasRiskSummary();
         
         cy.executeAccessibilityTests();
 
--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/siteInformation/viewSiteInformationPage.ts
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/pages/siteInformation/viewSiteInformationPage.ts
@@ -1,88 +1,156 @@
 export class ViewSiteInformationPage {
-    public hasTemporarySiteAddress(line1: string, line2: string, city: string): this {
-
-        cy.hasAddress("temporary-site-address", line1, line2, city);
-
-        return this;
-    }
-
-    public hasTemporarySitePostcode(postcode: string): this {
-        cy.getByTestId("temporary-site-postcode").should("contain.text", postcode);
+    public hasTemporaryAddress(): this {
+        cy.getById("temporary-address").should("be.visible");
 
         return this;
     }
 
-    public hasTemporarySiteDatePlanningPermissionObtained(value: string): this {
-        cy.getByTestId("temporary-site-date-planning-permission-obtained").should("contain.text", value);
+    public hasTemporaryPostcode(): this {
+        cy.getById("temporary-postcode").should("be.visible");
 
         return this;
     }
 
-    public hasTemporarySiteStartDateOfOccupation(value: string): this {
-        cy.getByTestId("temporary-site-start-date-of-site-occupation").should("contain.text", value);
+    public hasTemporarySiteRiskRating(): this {
+        cy.getById("temporary-site-risk-rating").should("be.visible");
 
         return this;
     }
 
-    public hasPermanentSiteAddress(line1: string, line2: string, city: string): this {
-        cy.hasAddress("permanent-site-address", line1, line2, city);
+    public hasTemporaryPlanningDecision(): this {
+        cy.getById("temporary-planning-decision").should("be.visible");
 
         return this;
     }
 
-    public hasPermanentSitePostcode(postcode: string): this {
-        cy.getByTestId("permanent-site-postcode").should("contain.text", postcode);
+    public hasTemporaryKeyDatesHeading(): this {
+        cy.getById("temporary-key-dates-heading").should("be.visible");
 
         return this;
     }
 
-    public hasPermanentSiteDatePlanningPermissionObtained(value: string): this {
-        cy.getByTestId("permanent-site-date-planning-permission-obtained").should("contain.text", value);
+    public hasTemporaryKeyDates(): this {
+        cy.getById("temporary-key-dates").should("be.visible");
 
         return this;
     }
 
-    public hasPermanentSiteStartDateOfOccupation(value: string): this {
-        cy.getByTestId("permanent-site-start-date-of-site-occupation").should("contain.text", value);
+    public hasTemporaryForecastDates(): this {
+        cy.getById("temporary-forecast-dates").should("be.visible");
 
         return this;
     }
 
-    public hasSchoolName(value: string): this {
-        cy.getByTestId("school-name").should("contain.text", value);
+    public hasTemporaryActualDates(): this {
+        cy.getById("temporary-actual-dates").should("be.visible");
 
         return this;
     }
 
-    public changeTemporarySite(): this {
-        cy.getByTestId("change-temporary-site").click();
+    public hasTemporaryHeadsOfTermsAgreed(): this {
+        cy.getById("temporary-heads-of-terms-agreed").should("be.visible");
 
         return this;
     }
 
-    public changePermanentSite(): this {
-        cy.getByTestId("change-permanent-site").click();
+    public hasTemporaryContractorAppointed(): this {
+        cy.getById("temporary-contractor-appointed").should("be.visible");
+
+        return this;
+    }
+    public hasTemporaryPlanningDecisionDate(): this {
+        cy.getById("temporary-planning-decision-date").should("be.visible");
+
+        return this;
+    }
+    public hasTemporaryAccomodationFirstReadyForOccupation(): this {
+        cy.getById("temporary-accomodation-first-ready-for-occupation").should("be.visible");
 
         return this;
     }
 
-    public checkInsetTextExists() {
-        cy.getByTestId("central-route-hint").should("exist");
+    public hasPermanentAddress(): this {
+        cy.getById("permanent-address").should("be.visible");
 
         return this;
     }
 
-    public changeTemporarySiteShouldNotExist(): this {
-        cy.getById("change-temporary-site").should("not.exist");
+    public hasPermanentPostcode(): this {
+        cy.getById("permanent-postcode").should("be.visible");
 
         return this;
     }
 
-    public changePermanentSiteShouldNotExist(): this {
-        cy.getById("change-permanent-site").should("not.exist");
+    public hasPermanentSiteRiskRating(): this {
+        cy.getById("permanent-site-risk-rating").should("be.visible");
 
         return this;
     }
+
+    public hasPermanentPlanningDecision(): this {
+        cy.getById("permanent-planning-decision").should("be.visible");
+
+        return this;
+    }
+
+    public hasPermanentKeyDatesHeading(): this {
+        cy.getById("permanent-key-dates-heading").should("be.visible");
+
+        return this;
+    }
+
+    public hasPermanentKeyDates(): this {
+        cy.getById("permanent-key-dates").should("be.visible");
+
+        return this;
+    }
+
+    public hasPermanentForecastDates(): this {
+        cy.getById("permanent-forecast-dates").should("be.visible");
+
+        return this;
+    }
+
+    public hasPermanentActualDates(): this {
+        cy.getById("permanent-actual-dates").should("be.visible");
+
+        return this;
+    }
+
+    public hasPermanentHeadsOfTermsAgreed(): this {
+        cy.getById("permanent-heads-of-terms-agreed").should("be.visible");
+
+        return this;
+    }
+
+    public hasPermanentContractorAppointed(): this {
+        cy.getById("permanent-contractor-appointed").should("be.visible");
+
+        return this;
+    }
+    public hasPermanentPlanningDecisionDate(): this {
+        cy.getById("permanent-planning-decision-date").should("be.visible");
+
+        return this;
+    }
+    public hasPermanentAccomodationFirstReadyForOccupation(): this {
+        cy.getById("permanent-accomodation-first-ready-for-occupation").should("be.visible");
+
+        return this;
+    }
+
+    public hasCapitalRating(): this {
+        cy.getById("capital-rating").should("be.visible");
+
+        return this;
+    }
+    public hasRiskSummary(): this {
+        cy.getById("risk-summary").should("be.visible");
+
+        return this;
+    }
+
+   
 
     public backToProject(): this {
         cy.get(".govuk-back-link").click();
