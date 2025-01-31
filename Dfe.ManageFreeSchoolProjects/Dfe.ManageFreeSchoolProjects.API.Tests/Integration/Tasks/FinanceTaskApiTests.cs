c1161ebb4f5e5f77350fd40e4abe396d0c747268
Jack Marshall <jack.marshall@nimbleapproach.com>
2025-01-31 22:20:36 +0000

    Updated API tests

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API.Tests/Integration/Tasks/FinanceTaskApiTests.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API.Tests/Integration/Tasks/FinanceTaskApiTests.cs
@@ -28,6 +28,8 @@ namespace Dfe.ManageFreeSchoolProjects.API.Tests.Integration.Tasks
             var dateAgreed = DateTime.Now.Date.AddDays(-5);
             var rpaStartDate = DateTime.Now.Date.AddDays(10);
 
+            var comments = "Some comments";
+
             var request = new UpdateProjectByTaskRequest()
             {
                 FinancePlan = new FinancePlanTask()
@@ -38,7 +40,25 @@ namespace Dfe.ManageFreeSchoolProjects.API.Tests.Integration.Tasks
                     PlanSavedInWorkplacesFolder = YesNo.Yes,
                     TrustWillOptIntoRpa = YesNo.Yes,
                     RpaCoverType = "RpaCoverType",
-                    RpaStartDate = rpaStartDate
+                    RpaStartDate = rpaStartDate,
+                    UnderwrittenPlacesPrimaryYear1 = 1,
+                    UnderwrittenPlacesPrimaryYear2 = 2,
+                    UnderwrittenPlacesPrimaryYear3 = 3,
+                    UnderwrittenPlacesPrimaryYear4 = 4,
+                    UnderwrittenPlacesPrimaryYear5 = 5,
+                    UnderwrittenPlacesPrimaryYear6 = 6,
+                    UnderwrittenPlacesPrimaryYear7 = 7,
+                    UnderwrittenPlacesSecondaryYear1 = 8,
+                    UnderwrittenPlacesSecondaryYear2 = 9,
+                    UnderwrittenPlacesSecondaryYear3 = 10,
+                    UnderwrittenPlacesSecondaryYear4 = 11,
+                    UnderwrittenPlacesSecondaryYear5 = 12,
+                    UnderwrittenPlacesSixteenToNineteenYear1 = 13,
+                    UnderwrittenPlacesSixteenToNineteenYear2 = 14,
+                    UnderwrittenPlacesSixteenToNineteenYear3 = 15,
+                    ConfirmationFromLocalAuthoritySavedInWorkplacesFolder = true,
+                    CommentsAboutUnderwrittenPlaces = comments
+
                 }
             };
 
@@ -51,6 +71,23 @@ namespace Dfe.ManageFreeSchoolProjects.API.Tests.Integration.Tasks
             projectResponse.FinancePlan.TrustWillOptIntoRpa.Should().Be(YesNo.Yes);
             projectResponse.FinancePlan.RpaCoverType.Should().Be("RpaCoverType");
             projectResponse.FinancePlan.RpaStartDate.Should().Be(rpaStartDate);
+            projectResponse.FinancePlan.UnderwrittenPlacesPrimaryYear1.Should().Be(1);
+            projectResponse.FinancePlan.UnderwrittenPlacesPrimaryYear2.Should().Be(2);
+            projectResponse.FinancePlan.UnderwrittenPlacesPrimaryYear3.Should().Be(3);
+            projectResponse.FinancePlan.UnderwrittenPlacesPrimaryYear4.Should().Be(4);
+            projectResponse.FinancePlan.UnderwrittenPlacesPrimaryYear5.Should().Be(5);
+            projectResponse.FinancePlan.UnderwrittenPlacesPrimaryYear6.Should().Be(6);
+            projectResponse.FinancePlan.UnderwrittenPlacesPrimaryYear7.Should().Be(7);
+            projectResponse.FinancePlan.UnderwrittenPlacesSecondaryYear1.Should().Be(8);
+            projectResponse.FinancePlan.UnderwrittenPlacesSecondaryYear2.Should().Be(9);
+            projectResponse.FinancePlan.UnderwrittenPlacesSecondaryYear3.Should().Be(10);
+            projectResponse.FinancePlan.UnderwrittenPlacesSecondaryYear4.Should().Be(11);
+            projectResponse.FinancePlan.UnderwrittenPlacesSecondaryYear5.Should().Be(12);
+            projectResponse.FinancePlan.UnderwrittenPlacesSixteenToNineteenYear1.Should().Be(13);
+            projectResponse.FinancePlan.UnderwrittenPlacesSixteenToNineteenYear2.Should().Be(14);
+            projectResponse.FinancePlan.UnderwrittenPlacesSixteenToNineteenYear3.Should().Be(15);
+            projectResponse.FinancePlan.ConfirmationFromLocalAuthoritySavedInWorkplacesFolder.Should().Be(true);
+            projectResponse.FinancePlan.CommentsAboutUnderwrittenPlaces.Should().Be(comments);
         }
 
         [Fact]
@@ -70,6 +107,8 @@ namespace Dfe.ManageFreeSchoolProjects.API.Tests.Integration.Tasks
             var dateAgreed = DateTime.Now.Date.AddDays(-5);
             var rpaStartDate = DateTime.Now.Date.AddDays(10);
 
+            var comments = "Some comments";
+
             var request = new UpdateProjectByTaskRequest()
             {
                 FinancePlan = new FinancePlanTask()
@@ -80,7 +119,24 @@ namespace Dfe.ManageFreeSchoolProjects.API.Tests.Integration.Tasks
                     PlanSavedInWorkplacesFolder = YesNo.No,
                     TrustWillOptIntoRpa = YesNo.No,
                     RpaCoverType = "a new RpaCoverType",
-                    RpaStartDate = rpaStartDate
+                    RpaStartDate = rpaStartDate,
+                    UnderwrittenPlacesPrimaryYear1 = 1,
+                    UnderwrittenPlacesPrimaryYear2 = 2,
+                    UnderwrittenPlacesPrimaryYear3 = 3,
+                    UnderwrittenPlacesPrimaryYear4 = 4,
+                    UnderwrittenPlacesPrimaryYear5 = 5,
+                    UnderwrittenPlacesPrimaryYear6 = 6,
+                    UnderwrittenPlacesPrimaryYear7 = 7,
+                    UnderwrittenPlacesSecondaryYear1 = 8,
+                    UnderwrittenPlacesSecondaryYear2 = 9,
+                    UnderwrittenPlacesSecondaryYear3 = 10,
+                    UnderwrittenPlacesSecondaryYear4 = 11,
+                    UnderwrittenPlacesSecondaryYear5 = 12,
+                    UnderwrittenPlacesSixteenToNineteenYear1 = 13,
+                    UnderwrittenPlacesSixteenToNineteenYear2 = 14,
+                    UnderwrittenPlacesSixteenToNineteenYear3 = 15,
+                    ConfirmationFromLocalAuthoritySavedInWorkplacesFolder = true,
+                    CommentsAboutUnderwrittenPlaces = comments
                 }
             };
 
@@ -93,6 +149,23 @@ namespace Dfe.ManageFreeSchoolProjects.API.Tests.Integration.Tasks
             projectResponse.FinancePlan.TrustWillOptIntoRpa.Should().Be(YesNo.No);
             projectResponse.FinancePlan.RpaCoverType.Should().Be("a new RpaCoverType");
             projectResponse.FinancePlan.RpaStartDate.Should().Be(rpaStartDate);
+            projectResponse.FinancePlan.UnderwrittenPlacesPrimaryYear1.Should().Be(1);
+            projectResponse.FinancePlan.UnderwrittenPlacesPrimaryYear2.Should().Be(2);
+            projectResponse.FinancePlan.UnderwrittenPlacesPrimaryYear3.Should().Be(3);
+            projectResponse.FinancePlan.UnderwrittenPlacesPrimaryYear4.Should().Be(4);
+            projectResponse.FinancePlan.UnderwrittenPlacesPrimaryYear5.Should().Be(5);
+            projectResponse.FinancePlan.UnderwrittenPlacesPrimaryYear6.Should().Be(6);
+            projectResponse.FinancePlan.UnderwrittenPlacesPrimaryYear7.Should().Be(7);
+            projectResponse.FinancePlan.UnderwrittenPlacesSecondaryYear1.Should().Be(8);
+            projectResponse.FinancePlan.UnderwrittenPlacesSecondaryYear2.Should().Be(9);
+            projectResponse.FinancePlan.UnderwrittenPlacesSecondaryYear3.Should().Be(10);
+            projectResponse.FinancePlan.UnderwrittenPlacesSecondaryYear4.Should().Be(11);
+            projectResponse.FinancePlan.UnderwrittenPlacesSecondaryYear5.Should().Be(12);
+            projectResponse.FinancePlan.UnderwrittenPlacesSixteenToNineteenYear1.Should().Be(13);
+            projectResponse.FinancePlan.UnderwrittenPlacesSixteenToNineteenYear2.Should().Be(14);
+            projectResponse.FinancePlan.UnderwrittenPlacesSixteenToNineteenYear3.Should().Be(15);
+            projectResponse.FinancePlan.ConfirmationFromLocalAuthoritySavedInWorkplacesFolder.Should().Be(true);
+            projectResponse.FinancePlan.CommentsAboutUnderwrittenPlaces.Should().Be(comments);
         }
     }
 }
