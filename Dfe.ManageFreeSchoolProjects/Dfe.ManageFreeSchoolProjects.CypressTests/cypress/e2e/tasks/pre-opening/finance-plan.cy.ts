eb48f10263872b17eadf16e0f99505b58a0fd81d
Jack Marshall <jack.marshall@nimbleapproach.com>
2025-02-03 17:37:04 +0000

    Updated Cypress tests

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/e2e/tasks/pre-opening/finance-plan.cy.ts
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/e2e/tasks/pre-opening/finance-plan.cy.ts
@@ -5,6 +5,7 @@ import { Logger } from "cypress/common/logger";
 import summaryPage from "cypress/pages/task-summary-base";
 import taskListPage from "cypress/pages/taskListPage";
 import editFinancePlanPage from "cypress/pages/tasks/pre-opening/editFinancePlanPage";
+import editUnderwrittenPlacesPage from "cypress/pages/tasks/pre-opening/editUnderwrittenPlacesPage";
 import validationComponent from "cypress/pages/validationComponent";
 
 describe("Testing finance plan task", () => {
@@ -49,7 +50,6 @@ describe("Testing finance plan task", () => {
             .summaryShows("Date agreed").IsEmpty().HasChangeLink()
             .summaryShows("Plan saved in Workplaces folder").IsEmpty().HasChangeLink()
             .summaryShows("Local authority agreed to underwrite pupil numbers").IsEmpty().HasChangeLink()
-            .summaryShows("Comments").IsEmpty().HasChangeLink()
             .summaryShows("Trust will opt-in to RPA (risk protection arrangement)").IsEmpty().HasChangeLink()
             .isNotMarkedAsComplete();
 
@@ -72,13 +72,11 @@ describe("Testing finance plan task", () => {
             .withDateAgreed("33", "", "")
             .withRpaStartDate("44", "", "")
             .withCoverTypeExceedingMaxLength()
-            .withCommentsExceedingMaxLength()
             .clickContinue();
 
         validationComponent
             .hasValidationError("Date agreed must include a month and year")
             .hasValidationError("RPA start date must include a month and year")
-            .hasValidationError("Comments must be 999 characters or less")
             .hasLinkedValidationError("Type of RPA cover must be 100 characters or less");
 
         // The conditional radio buttons break "aria-allowed-attr"
@@ -90,8 +88,7 @@ describe("Testing finance plan task", () => {
             .checkFinancePlanAgreed()
             .withDateAgreed("10", "08", "2024")
             .checkPlanSavedInWorkplacesFolder()
-            .withLocalAuthorityAgreedToUnderwritePupilNumbers("Yes")
-            .withComments("Some comments")
+            .withLocalAuthorityAgreedToUnderwritePupilNumbers("No")
             .withTrustWillOptInToRpa("Yes")
             .withRpaStartDate("12", "09", "2025")
             .withRpaCoverType("Comprehensive")
@@ -102,8 +99,7 @@ describe("Testing finance plan task", () => {
             .summaryShows("Finance plan agreed").HasValue("Yes")
             .summaryShows("Date agreed").HasValue("10 August 2024")
             .summaryShows("Plan saved in Workplaces folder").HasValue("Yes")
-            .summaryShows("Local authority agreed to underwrite pupil numbers").HasValue("Yes")
-            .summaryShows("Comments").HasValue("Some comments")
+            .summaryShows("Local authority agreed to underwrite pupil numbers").HasValue("No")
             .summaryShows("Trust will opt-in to RPA (risk protection arrangement)").HasValue("Yes")
             .summaryShows(rpaStartDateLabel).HasValue("12 September 2025")
             .summaryShows(rpaCoverTypeLabel).HasValue("Comprehensive");
@@ -116,7 +112,6 @@ describe("Testing finance plan task", () => {
             .withDateAgreed("12", "05", "2027")
             .checkPlanSavedInWorkplacesFolder()
             .withLocalAuthorityAgreedToUnderwritePupilNumbers("NotApplicable")
-            .withComments("This is my new comments")
             .withTrustWillOptInToRpa("Yes")
             .withRpaStartDate("13", "06", "2026")
             .withRpaCoverType("Standard")
@@ -128,7 +123,6 @@ describe("Testing finance plan task", () => {
             .summaryShows("Date agreed").HasValue("12 May 2027")
             .summaryShows("Plan saved in Workplaces folder").HasValue("No")
             .summaryShows("Local authority agreed to underwrite pupil numbers").HasValue("Not applicable")
-            .summaryShows("Comments").HasValue("This is my new comments")
             .summaryShows("Trust will opt-in to RPA (risk protection arrangement)").HasValue("Yes")
             .summaryShows(rpaStartDateLabel).HasValue("13 June 2026")
             .summaryShows(rpaCoverTypeLabel).HasValue("Standard");
@@ -145,7 +139,7 @@ describe("Testing finance plan task", () => {
 
         Logger.log("Should not show the RPA fields if no is selected and the optional fields should not be displayed")
         summaryPage
-            .startFromRow(5)
+            .startFromRow(4)
             .summaryShows("Trust will opt-in to RPA (risk protection arrangement)").HasValue("No")
             .summaryDoesNotShow(rpaStartDateLabel)
             .summaryDoesNotShow(rpaCoverTypeLabel);
@@ -154,10 +148,40 @@ describe("Testing finance plan task", () => {
         summaryPage.clickChange();
 
         editFinancePlanPage
+            .withLocalAuthorityAgreedToUnderwritePupilNumbers("Yes")
             .withTrustWillOptInToRpa("Yes")
             .hasCoverType("")
             .hasRpaStartDate("", "", "")
             .clickContinue();
+
+        Logger.log("Should open underwriting places page");
+        editUnderwrittenPlacesPage
+            .schoolNameIs(project.schoolName)
+            .withPrimaryYear1Places("111")
+            .withPrimaryYear3Places("222")
+            .withPrimaryYear7Places("333")
+            .checkConfirmationFromLASavedInWorkplacesFolder()
+            .withCommentsExceedingMaxLength()
+            .clickContinue();
+
+        validationComponent
+            .hasValidationError("Add comments about underwritten places must be 500 characters or less");
+
+        editUnderwrittenPlacesPage
+            .withComments("Some comments")
+            .clickContinue();
+
+        Logger.log("Should show underwritten places")
+            summaryPage
+                .startFromRow(4)
+                .summaryShows("Number of underwritten primary places").HasValue("First year of opening: 111")
+                .startFromRow(4)
+                .summaryShows("Number of underwritten primary places").HasValue("Third year of opening: 222")
+                .startFromRow(4)
+                .summaryShows("Number of underwritten primary places").HasValue("Seventh year of opening: 333")
+                .skip(2)
+                .summaryShows("Confirmation from local authority saved in workplaces folder").HasValue("Yes")
+                .summaryShows("Comments about underwritten places").HasValue("Some comments");
 
         Logger.log("Should update the task status");
         summaryPage.clickConfirmAndContinue();
