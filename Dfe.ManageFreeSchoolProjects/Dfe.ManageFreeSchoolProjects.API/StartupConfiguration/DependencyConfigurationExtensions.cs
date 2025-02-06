DFE-Digital/manage-free-school-projects

Backend and services for splitting central and presumption route for site information

Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API/StartupConfiguration/DependencyConfigurationExtensions.cs
@@ -7,7 +7,6 @@ using Dfe.ManageFreeSchoolProjects.API.UseCases.Project;
 using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Contacts;
 using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.PupilNumbers;
 using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Risk;
-using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Sites;
 using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Tasks;
 using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Tasks.AdmissionsArrangements;
 using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Tasks.ApplicationsEvidence;
@@ -59,6 +58,7 @@ using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Tasks.PreFundingAgreemen
 using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Tasks.ReadinessToOpenMeeting;
 using Dfe.ManageFreeSchoolProjects.API.UseCases.BulkEdit;
 using Dfe.ManageFreeSchoolProjects.API.UseCases.LocalAuthority;
+using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Sites.Central;
 
 namespace Dfe.ManageFreeSchoolProjects.API.StartupConfiguration
 {
@@ -132,7 +132,7 @@ namespace Dfe.ManageFreeSchoolProjects.API.StartupConfiguration
 			services.AddScoped<ISfaReportService, SfaReportService>();
 			services.AddScoped<IUpdateTaskService, UpdateAdmissionsArrangementsTaskService>();
 			services.AddScoped<ISfaApiKeyValidationService, SfaApiKeyValidationService>();
-			services.AddScoped<IGetProjectSitesService, GetProjectSitesService>();
+			services.AddScoped<IGetProjectSitesService, GetProjectSitesCentralService>();
 			services.AddScoped<IGetPupilNumbersService, GetPupilNumbersService>();
 			services.AddScoped<IUpdatePupilNumbersService, UpdatePupilNumbersService>();
 			services.AddScoped<IUpdateCapacityWhenFullService, UpdateCapacityWhenFullService>();

Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Startup.cs
@@ -109,7 +109,7 @@ public class Startup
         services.AddScoped<IGetProjectManagersService, GetProjectManagersService>();
         services.AddScoped<IAnalyticsConsentService, AnalyticsConsentService>();
         services.AddScoped<IAllProjectsReportService, AllProjectsReportService>();
-        services.AddScoped<IGetProjectSitesService, GetProjectSitesService>();
+        services.AddScoped<IGetProjectSitesService, GetProjectSitesCentralService>();
         services.AddScoped<IGetPupilNumbersService, GetPupilNumbersService>();
         services.AddScoped<IUpdatePupilNumbersService, UpdatePupilNumbersService>();
         services.AddScoped<IGetProjectPaymentsService, GetProjectPaymentsService>();

DFE-Digital/manage-free-schools-projects

Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.API/StartupConfiguration/DependencyConfigurationExtensions.cs
@@ -7,7 +7,6 @@ using Dfe.ManageFreeSchoolProjects.API.UseCases.Project;
 using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Contacts;
 using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.PupilNumbers;
 using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Risk;
-using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Sites;
 using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Tasks;
 using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Tasks.AdmissionsArrangements;
 using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Tasks.ApplicationsEvidence;
@@ -59,6 +58,7 @@ using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Tasks.PreFundingAgreemen
 using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Tasks.ReadinessToOpenMeeting;
 using Dfe.ManageFreeSchoolProjects.API.UseCases.BulkEdit;
 using Dfe.ManageFreeSchoolProjects.API.UseCases.LocalAuthority;
+using Dfe.ManageFreeSchoolProjects.API.UseCases.Project.Sites;
 
 namespace Dfe.ManageFreeSchoolProjects.API.StartupConfiguration
 {
@@ -132,8 +132,10 @@ namespace Dfe.ManageFreeSchoolProjects.API.StartupConfiguration
 			services.AddScoped<ISfaReportService, SfaReportService>();
 			services.AddScoped<IUpdateTaskService, UpdateAdmissionsArrangementsTaskService>();
 			services.AddScoped<ISfaApiKeyValidationService, SfaApiKeyValidationService>();
-			services.AddScoped<IGetProjectSitesService, GetProjectSitesService>();
-			services.AddScoped<IGetPupilNumbersService, GetPupilNumbersService>();
+			services.AddScoped<IGetProjectSitesCentralService, GetProjectSitesCentralService>();
+            services.AddScoped<IGetProjectSitesPresumptionService, GetProjectSitesPresumptionService>();
+            services.AddScoped<IUpdateProjectSitePresumptionService, UpdateProjectSitePresumptionService>();
+            services.AddScoped<IGetPupilNumbersService, GetPupilNumbersService>();
 			services.AddScoped<IUpdatePupilNumbersService, UpdatePupilNumbersService>();
 			services.AddScoped<IUpdateCapacityWhenFullService, UpdateCapacityWhenFullService>();
 			services.AddScoped<IUpdatePre16PublishedAdmissionNumberService, UpdatePre16PublishedAdmissionNumberService>();

Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/Startup.cs
@@ -109,7 +109,9 @@ public class Startup
         services.AddScoped<IGetProjectManagersService, GetProjectManagersService>();
         services.AddScoped<IAnalyticsConsentService, AnalyticsConsentService>();
         services.AddScoped<IAllProjectsReportService, AllProjectsReportService>();
-        services.AddScoped<IGetProjectSitesService, GetProjectSitesService>();
+        services.AddScoped<IGetProjectSitesCentralService, GetProjectSitesCentralService>();
+        services.AddScoped<IGetProjectSitesPresumptionService, GetProjectSitesPresumptionService>();
+        services.AddScoped<IUpdateProjectSitePresumptionService, UpdateProjectSitePresumptionService>();
         services.AddScoped<IGetPupilNumbersService, GetPupilNumbersService>();
         services.AddScoped<IUpdatePupilNumbersService, UpdatePupilNumbersService>();
         services.AddScoped<IGetProjectPaymentsService, GetProjectPaymentsService>();